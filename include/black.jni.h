11932645b8d2cc4f6b4083c8ea30f6ad2d1a0f9b
Frank Stein <dr.frank.stain@gmail.com>
2017-12-25 23:13:28 +0300

    New structure for master-header.

--- include/black.jni.h
+++ include/black.jni.h
@@ -1,56 +1,22 @@
-// Copyright since 2016 : Evgenii Shatunov (github.com/FrankStain/jnipp)
-// Apache 2.0 License
 #pragma once
 
+// Dependencies.
+#include <black.core.h>
 
-/// @brief	Jni++ main namespace.
-namespace Jni
+
+namespace Black
 {
-/// @brief	Logging support for Jni++. Only android log-cat used here by default.
-namespace Logging {};
+// Black::Jni library.
+inline namespace Jni
+{
 
-/// @brief	Useful utils for Jni++.
-namespace Utils {};
-
-/// @brief	Type marshaling for Jni++.
-namespace Marshaling {};
-
-/// @brief	Regular Java classes from Android SDK.
-namespace Android {};
+}
 }
 
-
-// Jni++ configuration.
-#include "core/configuration.h"
-
-// Most major system includes.
-#include <cstdint>
-#include <string>
-#include <typeinfo>
-#include <typeindex>
-#include <vector>
-#include <deque>
-#include <list>
-#include <set>
-#include <map>
-#include <unordered_map>
-#include <algorithm>
-#include <iterator>
-#include <memory>
-#include <jni.h>
-#include <android/log.h>
-
-// Jni++ core stuff.
-#include "core/logging.h"
-#include "core/macros.h"
-#include "core/types.h"
-
-// Jni++ utils.
-#include "utils/StaticString.h"
-#include "utils/StaticString.operations.h"
+// Most basic utils.
 #include "utils/HasBitsField.h"
 
-// Jni++ type marshaling.
+// Type Marshaling.
 #include "marshaling/JavaTypeTraits.h"
 #include "marshaling/EnvironmentTraits.h"
 #include "marshaling/ArrayTranslationTraits.h"
@@ -58,39 +24,39 @@ namespace Android {};
 #include "marshaling/EnumTypeTraits.h"
 #include "marshaling/BitfieldTypeTraits.h"
 
-// Jni++ forward declarations and useful types.
+// Most crucial forwards.
 #include "utils/forwards.h"
 #include "utils/types.h"
 
-// Jni++ wrap for Java classes.
+// JNI classes.
 #include "handles/Class.h"
 #include "handles/Object.h"
 #include "marshaling/NativeTypeTraits.Object.h"
 #include "marshaling/NativeTypeTraits.Class.h"
 #include "marshaling/ObjectTypeTraits.h"
 
-// Jni++ wrap for Java class fields.
+// JNI member fields.
 #include "handles/MemberField.h"
 #include "handles/StaticField.h"
 
-// Jni++ wrap for Java functions.
+// JNI member functions.
 #include "utils/MemberFunctionInvocation.h"
 #include "utils/StaticFunctionInvocation.h"
 #include "handles/MemberFunction.h"
 #include "handles/StaticFunction.h"
 
-// Some crucial Jni++ types.
+// JNI native function specification.
 #include "utils/NativeFunction.h"
 #include "utils/NativeFunction.utils.h"
 #include "utils/NativeFunction.macros.h"
 
-// Jni++ major utility - virtual machine wrap.
+// JNI Virtual machine and environment.
 #include "utils/HandlesCacheEntry.h"
 #include "utils/CachedHandles.h"
 #include "utils/VirtualMachine.h"
 #include "utils/Environment.h"
 
-// Deferred implementation of Jni++ functionality.
+// Deferred implementation of JNI functionality.
 #include "utils/CachedHandles.inl"
 #include "utils/VirtualMachine.inl"
 #include "utils/Environment.inl"
