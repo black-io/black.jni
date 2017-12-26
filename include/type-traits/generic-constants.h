84f01fb2692617f9bfbd229913a060706bbd8a1b
Frank Stein <dr.frank.stain@gmail.com>
2017-12-26 10:51:16 +0300

    Refactoring of bit-fields check.

--- include/type-traits/generic-constants.h
+++ include/type-traits/generic-constants.h
@@ -1,18 +1,33 @@
-// Copyright since 2016 : Evgenii Shatunov (github.com/FrankStain/jnipp)
-// Apache 2.0 License
 #pragma once
 
 
-namespace Jni
+namespace Black
 {
-namespace Utils
+inline namespace Jni
 {
-	/// @brief	Check that the class `TBitfieldType` has member-field named `bits`.
-	template< typename TBitfieldType, typename = int >
-	struct HasBitsField : std::false_type {};
+inline namespace TypeTraits
+{
+namespace Traits
+{
+	// Common path to check the non-bit-field types.
+	template< typename TBitfield, typename = int >
+	struct HasBitsField : std::false_type
+	{
 
-	/// @brief	Check that the class `TBitfieldType` has member-field named `bits`.
-	template< typename TBitfieldType >
-	struct HasBitsField<TBitfieldType, decltype( (void)TBitfieldType::bits, 0 )> : std::true_type {};
+	};
+
+	// Path to check that the given type has required field for bit-fields.
+	template< typename TBitfield >
+	struct HasBitsField<TBitfield, decltype( (void)TBitfield::bits, 0 )> : std::true_type
+	{
+
+	};
+}
+
+
+	// Bit-field type check.
+	template< typename TCondition >
+	constexpr bool IS_BIT_FIELD = Traits::HasBitsField<TCondition>::value;
+}
 }
 }
