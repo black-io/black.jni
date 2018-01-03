e060d2a0830f8709df5ec8a515b161e0db15a9d4
Frank Stein <dr.frank.stain@gmail.com>
2018-01-03 10:51:04 +0300

    JNI type context for native enumeration.

--- include/marshaling/NativeEnumContext.h
+++ include/marshaling/NativeEnumContext.h
@@ -1,48 +1,57 @@
-// Copyright since 2016 : Evgenii Shatunov (github.com/FrankStain/jnipp)
-// Apache 2.0 License
 #pragma once
 
 
-namespace Jni
+namespace Black
 {
-namespace Marshaling
+inline namespace Jni
 {
-	/// @brief	Type translation traits specification for enumeration types.
-	template< typename TEnumType, bool IS_ENUM = std::is_enum<TEnumType>::value >
-	struct EnumTypeTraits;
+inline namespace Marshaling
+{
+namespace Traits
+{
+	// Safe environment context for native enumeration.
+	template< typename TNativeEnum, bool IS_VALID_ENUMERATION >
+	struct EnumContext;
 
-	/// @brief	Type translation traits specification for enumeration types.
-	template< typename TEnumType >
-	struct EnumTypeTraits<TEnumType, true> : NativeTypeTraits<typename std::underlying_type<TEnumType>::type>
+	// JNI environment context for native enumeration.
+	template< typename TNativeEnum >
+	struct EnumContext<TNativeEnum, true> : Black::NativeTypeContext<Black::UnderlyingType<TNativeEnum>>
 	{
-		/// @brief	Underlying type for enumeration type.
-		using UnderlyingType	= typename std::underlying_type<TEnumType>::type;
+		// Underlying type for enumeration type.
+		using UnderlyingType	= typename std::underlying_type<TNativeEnum>::type;
 
-		/// @brief	Native type traits for underlying type of enumeration type.
-		using UnderlyingTraits	= NativeTypeTraits<UnderlyingType>;
-
-
-		/// @brief	C++ native type.
-		using NativeType	= TEnumType;
-
-		/// @brief	JNI representation of Java type.
-		using JavaType		= typename UnderlyingTraits::JavaType;
+		// Native type traits for underlying type of enumeration type.
+		using UnderlyingContext	= Black::NativeTypeContext<UnderlyingType>;
 
 
-		/// @brief	Type translation from Java space to C++ space.
-		static inline void FromJava( const JavaType& source, NativeType& destination )
+		// C++ native type.
+		using NativeType	= TNativeEnum;
+
+		// JNI type
+		using JniType		= typename UnderlyingContext::JniType;
+
+
+		// Type translation from JNI space to C++ space.
+		static inline void FromJava( const JniType& source, NativeType& destination )
 		{
 			UnderlyingType buffer;
-			UnderlyingTraits::FromJava( source, buffer );
-			destination = static_cast<TEnumType>( buffer );
+			UnderlyingContext::FromJava( source, buffer );
+			destination = static_cast<TNativeEnum>( buffer );
 		}
 
-		/// @brief	Type translation from C++ space to Java space.
-		static inline void ToJava( const NativeType& source, JavaType& destination )
+		// Type translation from C++ space to JNI space.
+		static inline void ToJava( const NativeType& source, JniType& destination )
 		{
 			const UnderlyingType buffer = static_cast<UnderlyingType>( source );
-			UnderlyingTraits::FromJava( buffer, destination );
+			UnderlyingContext::FromJava( buffer, destination );
 		}
 	};
+}
+
+
+	// JNI environment context for native enumerations.
+	template< typename TNativeEnum >
+	using NativeEnumContext = Traits::EnumContext<TNativeEnum, Black::IS_ENUMERATION<TNativeEnum>>;
+}
 }
 }
