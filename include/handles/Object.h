d2a4e7a2530311e36b1cbb4fc9db6afa01982d78
Frank Stein <dr.frank.stain@gmail.com>
2018-01-05 11:59:30 +0300

    JNI object handle.

--- include/handles/Object.h
+++ include/handles/Object.h
@@ -1,69 +1,78 @@
-// Copyright since 2016 : Evgenii Shatunov (github.com/FrankStain/jnipp)
-// Apache 2.0 License
 #pragma once
 
 
-namespace Jni
+namespace Black
 {
-	/// @brief	Handle of arbitrary Java object.
-	class Object
+inline namespace Jni
+{
+inline namespace Handles
+{
+	// Handle of arbitrary Java object.
+	class JniObject
 	{
+	// Construction and assignation.
 	public:
-		/// @brief	Constructs new object of given class and using given construction arguments.
-		template< typename... TNativeArguments >
-		static Object NewObject( const Class& class_handle, const TNativeArguments&... arguments );
-
-		Object() = default;
-		Object( jobject object_ref );
-		Object( const Class& class_handle );
-		Object( Class&& class_handle );
-		Object( const Object& other );
-		Object( Object&& other );
+		// Construct new object of given class and using given construction arguments.
+		template< typename... TArguments >
+		static JniObject Construct( const JniClass& class_handle, const TArguments&... arguments );
 
 
-		/// @brief	Invalidate the handle.
+		JniObject() = default;
+		JniObject( jobject object_ref );
+		JniObject( const JniClass& class_handle );
+		JniObject( JniClass&& class_handle );
+		JniObject( const JniObject& other );
+		JniObject( JniObject&& other );
+
+
+		const JniObject& operator = ( jobject object_ref );
+		const JniObject& operator = ( const JniClass& class_handle );
+		const JniObject& operator = ( JniClass&& class_handle );
+		const JniObject& operator = ( const JniObject& other );
+		const JniObject& operator = ( JniObject&& other );
+
+	// Public interface.
+	public:
+		// Invalidate the handle.
 		void Invalidate();
 
-		/// @brief	Check that the object is instance of given class.
-		const bool IsInstanceOf( const Class& base_class ) const;
 
-		/// @brief	Check the object handle carries valid value.
+		// Check that the object is instance of given class.
+		const bool IsInstanceOf( const JniClass& base_class ) const;
+
+		// Check the object handle carries valid value.
 		inline const bool IsValid() const					{ return m_object_ref != nullptr; };
 
-		/// @brief	Get the handle to class of stored object.
-		inline const Class& GetClass() const				{ RetrieveClass(); return m_class_handle; };
+		// Get the handle to class of stored object.
+		inline const JniClass& GetJniClass() const			{ RetrieveClass(); return m_class_handle; };
 
-		/// @brief	Get the JNI representation of Java object reference.
+		// Get the JNI representation of Java object reference.
 		inline jobject GetJniReference() const				{ return m_object_ref.get(); };
-
-
-		const Object& operator = ( jobject object_ref );
-		const Object& operator = ( const Class& class_handle );
-		const Object& operator = ( Class&& class_handle );
-		const Object& operator = ( const Object& other );
-		const Object& operator = ( Object&& other );
 
 
 		inline explicit operator const bool () const		{ return IsValid(); };
 		inline jobject operator * () const					{ return GetJniReference(); };
 
+	// Private interface.
 	private:
-		/// @brief	Delete global ref to object.
+		// Delete the object reference.
 		static void DeleteObjectRef( jobject object_ref );
 
 
-		/// @brief	Lazy initialization of `m_class_handle`.
+		// Lazy initialization of `m_class_handle`.
 		void RetrieveClass() const;
 
-		/// @brief	Acquire the global ref to object.
+		// Acquire the global ref to object.
 		void AcquireObjectRef( jobject object_ref );
 
 	private:
 		std::shared_ptr<_jobject>	m_object_ref;	// Shared JNI representation of Java object global reference.
-		mutable Class				m_class_handle;	// Handle to class of stored object.
+		mutable JniClass			m_class_handle;	// Handle to class of stored object.
 	};
 
 
-	const bool operator == ( const Object& left, const Object& right );
-	const bool operator != ( const Object& left, const Object& right );
+	const bool operator == ( const JniObject& left, const JniObject& right );
+	const bool operator != ( const JniObject& left, const JniObject& right );
+}
+}
 }
