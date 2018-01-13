d325532585be3a80f567c2a6dc1f5fda7d5bb696
Frank Stein <dr.frank.stain@gmail.com>
2018-01-13 23:00:03 +0300

    JNI member-function entry point.

--- include/handles/MemberFunctionInvocation.h
+++ include/handles/MemberFunctionInvocation.h
@@ -1,58 +1,69 @@
-// Copyright since 2016 : Evgenii Shatunov (github.com/FrankStain/jnipp)
-// Apache 2.0 License
 #pragma once
 
 
-namespace Jni
+namespace Black
 {
-namespace Utils
+inline namespace Jni
 {
-	/// @brief	Invocation of Java member-function. Directly used by `MemberFunction` to invoke the Java code.
-	template< typename TNativeReturnType, typename... TNativeArguments >
-	class MemberFunctionInvocation
+inline namespace Handles
+{
+namespace Traits
+{
+	// Entry-point for JNI member function.
+	template< typename TResult, typename... TArguments >
+	class MemberFunctionEntry
 	{
 	public:
-		inline MemberFunctionInvocation( JNIEnv* local_env, jobject object_ref, jmethodID function_id );
+		inline MemberFunctionEntry( JNIEnv* local_env, jobject object_ref, jmethodID function_id );
 
-		/// @brief	Call the Java method on given Java object with given function arguments.
-		inline TNativeReturnType Call( const TNativeArguments&... arguments ) const;
+		// Execute the bound function using regular virtual invocation.
+		inline TResult Call( const TArguments&... arguments ) const;
 
-		/// @brief	Call the non-virtual Java method on given Java object with given function arguments.
-		inline TNativeReturnType CallNonVirtual( jclass class_ref, const TNativeArguments&... arguments ) const;
-
-	private:
-		/// @brief	Java type of function call result.
-		using JavaType	= typename Jni::Marshaling::JavaType<TNativeReturnType>;
+		// Execute the bound function directly from class of object.
+		inline TResult CallNonVirtual( const TArguments&... arguments ) const;
 
 	private:
-		constexpr static auto FUNCTION_HANDLER				= Jni::Marshaling::NativeTypeTraits<TNativeReturnType>::FUNCTION_HANDLER;
-		constexpr static auto NONVIRTUAL_FUNCTION_HANDLER	= Jni::Marshaling::NativeTypeTraits<TNativeReturnType>::NONVIRTUAL_FUNCTION_HANDLER;
+		// Type of function result from JNI side.
+		using JniResult = Black::JniType<TResult>;
+
+		// JNI environment context.
+		using JniContext	= Black::NativeTypeContext<TNativeType>;
+
+	private:
+		constexpr static auto FUNCTION_HANDLER				= JniContext::FUNCTION_HANDLER;
+		constexpr static auto NONVIRTUAL_FUNCTION_HANDLER	= JniContext::NONVIRTUAL_FUNCTION_HANDLER;
 
 		JNIEnv*		m_local_env		= nullptr;	// Current thread-local JNI environment.
 		jmethodID	m_function_id	= nullptr;	// Id of function.
 		jobject		m_object_ref	= nullptr;	// Reference to Java object.
 	};
 
-	/// @brief	Invocation of Java member-function with `void` result type. Directly used by `MemberFunction` to invoke the Java code.
-	template< typename... TNativeArguments >
-	class MemberFunctionInvocation<void, TNativeArguments...>
+	// Entry-point for JNI member function with void result.
+	template< typename... TArguments >
+	class MemberFunctionEntry< void, TArguments... >
 	{
 	public:
-		inline MemberFunctionInvocation( JNIEnv* local_env, jobject object_ref, jmethodID function_id );
+		inline MemberFunctionEntry( JNIEnv* local_env, jobject object_ref, jmethodID function_id );
 
-		/// @brief	Call the Java method on given Java object with given function arguments.
-		inline void Call( const TNativeArguments&... arguments ) const;
+		// Execute the bound function using regular virtual invocation.
+		inline void Call( const TArguments&... arguments ) const;
 
-		/// @brief	Call the non-virtual Java method on given Java object with given function arguments.
-		inline void CallNonVirtual( jclass class_ref, const TNativeArguments&... arguments ) const;
+		// Execute the bound function directly from class of object.
+		inline void CallNonVirtual( const TArguments&... arguments ) const;
 
 	private:
-		constexpr static auto FUNCTION_HANDLER				= Jni::Marshaling::NativeTypeTraits<void>::FUNCTION_HANDLER;
-		constexpr static auto NONVIRTUAL_FUNCTION_HANDLER	= Jni::Marshaling::NativeTypeTraits<void>::NONVIRTUAL_FUNCTION_HANDLER;
+		// JNI environment context.
+		using JniContext	= Black::NativeTypeContext<void>;
+
+	private:
+		constexpr static auto FUNCTION_HANDLER				= JniContext::FUNCTION_HANDLER;
+		constexpr static auto NONVIRTUAL_FUNCTION_HANDLER	= JniContext::NONVIRTUAL_FUNCTION_HANDLER;
 
 		JNIEnv*		m_local_env		= nullptr;	// Current thread-local JNI environment.
 		jmethodID	m_function_id	= nullptr;	// Id of function.
 		jobject		m_object_ref	= nullptr;	// Reference to Java object.
 	};
+}
+}
 }
 }
