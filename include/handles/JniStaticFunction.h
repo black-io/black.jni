d0c693c3b706be508a807caa401eac14a2898782
Frank Stein <dr.frank.stain@gmail.com>
2018-01-14 21:44:25 +0300

    Handle for JNI static functions.

--- include/handles/JniStaticFunction.h
+++ include/handles/JniStaticFunction.h
@@ -1,60 +1,71 @@
-// Copyright since 2016 : Evgenii Shatunov (github.com/FrankStain/jnipp)
-// Apache 2.0 License
 #pragma once
 
 
-namespace Jni
+namespace Black
 {
-	/// @brief	Handle of arbitrary Java static function.
-	template< typename TNativeReturnType, typename... TNativeArguments >
-	class StaticFunction final
+inline namespace Jni
+{
+inline namespace Handles
+{
+	// Static function handle for JNI classes.
+	template< typename TResult, typename... TArguments >
+	class JniStaticFunction final
 	{
-		friend class Environment;
+		friend class Black::Jni::VirtualMachine::JniEnvironment; // Grant access to function calls.
+
+	// Construction and assignment.
 	public:
-		StaticFunction() = default;
-		StaticFunction( const StaticFunction& other );
-		StaticFunction( StaticFunction&& other );
-		StaticFunction( const std::string& class_name, const std::string& function_name );
-		StaticFunction( const Class& class_handle, const std::string& function_name );
-		StaticFunction( const char* class_name, const char* function_name );
-		StaticFunction( const Class& class_handle, const char* function_name );
+		JniStaticFunction() = default;
 
-		StaticFunction( const std::string& class_name, const std::string& function_name, IgnoreFailure );
-		StaticFunction( const Class& class_handle, const std::string& function_name, IgnoreFailure );
-		StaticFunction( const char* class_name, const char* function_name, IgnoreFailure );
-		StaticFunction( const Class& class_handle, const char* function_name, IgnoreFailure );
+		JniStaticFunction( const JniStaticFunction& other );
+		JniStaticFunction( JniStaticFunction&& other );
+
+		JniStaticFunction( Black::StringView class_name, Black::StringView function_name );
+		JniStaticFunction( const Black::JniClass& class_handle, Black::StringView function_name );
+
+		JniStaticFunction( Black::StringView class_name, Black::StringView function_name, Black::IgnoreFailure );
+		JniStaticFunction( const Black::JniClass& class_handle, Black::StringView function_name, Black::IgnoreFailure );
 
 
-		/// @brief	Call the function with given arguments.
-		inline TNativeReturnType Call( const TNativeArguments&... arguments ) const;
+		inline JniStaticFunction& operator = ( const JniStaticFunction& other );
+		inline JniStaticFunction& operator = ( JniStaticFunction&& other );
+
+	// Public interface.
+	public:
+		// Call the function using given arguments.
+		inline TResult Call( const TArguments&... arguments ) const;
 
 
-		/// @brief	Check the field handle carries valid value.
-		inline const bool IsValid() const				{ return m_class_handle.IsValid() && ( m_function_id != 0 ); };
+		// Check the field handle carries valid value.
+		inline const bool IsValid() const				{ return m_function_id != 0; };
 
-		/// @brief	Get the JNI id of Java static method.
+		// Get the function id.
 		inline jmethodID GetFunctionId() const			{ return m_function_id; };
 
-		/// @brief	Get the function signature string.
-		inline const char* GetSignature() const			{ return Signature::GetString(); };
-
-
-		inline const StaticFunction& operator = ( const StaticFunction& other );
-		inline const StaticFunction& operator = ( StaticFunction&& other );
+		// Get the signature of function.
+		inline Black::StringView GetSignature() const	{ return { Signature::GetData(), Signature::GetLength() }; };
 
 
 		inline explicit operator const bool () const	{ return IsValid(); };
 		inline jmethodID operator * () const			{ return GetFunctionId(); };
 
+	// Private interface.
 	private:
-		using Signature				= FunctionSignature< Marshaling::TypeSignature<TNativeReturnType>, Marshaling::TypeSignature<TNativeArguments>... >;
-		using FunctionInvocation	= Utils::StaticFunctionInvocation<TNativeReturnType, TNativeArguments...>;
+		// JNI type signature.
+		using Signature		= Black::NativeTypeSignature<TNativeType>;
 
-		/// @brief	Call the function with given arguments.
-		inline TNativeReturnType Call( JNIEnv* local_env, const TNativeArguments&... arguments ) const;
+		// Entry-point.
+		using EntryPoint	= Traits::MemberFunctionEntry<TResult, TArguments...>;
 
+
+		// Call the function using given arguments and local JNI environment.
+		inline TResult Call( JNIEnv* local_env, const TArguments&... arguments ) const;
+
+	// Private state.
 	private:
-		Class		m_class_handle;				// Handle to class owning the static method.
-		jmethodID	m_function_id	= nullptr;	// Id of Java static method.
+		Black::JniClass	m_class_handle;				// Handle to class owning the static method.
+		jmethodID		m_function_id	= nullptr;	// Id of Java object method.
 	};
+}
+}
 }
