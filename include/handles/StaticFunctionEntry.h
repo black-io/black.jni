5991ba3aa9d40cec27fe6a1dec0423439b43b55c
Frank Stein <dr.frank.stain@gmail.com>
2018-01-14 14:25:05 +0300

    Entry point for JNI static functions.

--- include/handles/StaticFunctionEntry.h
+++ include/handles/StaticFunctionEntry.h
@@ -1,50 +1,61 @@
-// Copyright since 2016 : Evgenii Shatunov (github.com/FrankStain/jnipp)
-// Apache 2.0 License
 #pragma once
 
 
-namespace Jni
+namespace Black
 {
-namespace Utils
+inline namespace Jni
 {
-	/// @brief	Invocation of Java static function. Directly used by `MemberFunction` to invoke the Java code.
-	template< typename TNativeReturnType, typename... TNativeArguments >
-	class StaticFunctionInvocation
+inline namespace Handles
+{
+namespace Traits
+{
+	// Entry-point for JNI static function.
+	template< typename TResult, typename... TArguments >
+	class StaticFunctionEntry
 	{
 	public:
-		inline StaticFunctionInvocation( JNIEnv* local_env, jclass class_ref, jmethodID function_id );
+		inline StaticFunctionEntry( JNIEnv* local_env, jclass class_ref, jmethodID function_id );
 
-		/// @brief	Call the Java method on given Java class with given function arguments.
-		inline TNativeReturnType Call( const TNativeArguments&... arguments ) const;
-
-	private:
-		/// @brief	Java type of function call result.
-		using JavaType	= typename Jni::Marshaling::JavaType<TNativeReturnType>;
+		// Execute the bound function.
+		inline TResult Call( const TArguments&... arguments ) const;
 
 	private:
-		constexpr static auto FUNCTION_HANDLER	= Jni::Marshaling::NativeTypeTraits<TNativeReturnType>::STATIC_FUNCTION_HANDLER;
+		// Type of function result from JNI side.
+		using JniResult		= Black::JniType<TResult>;
+
+		// JNI environment context.
+		using JniContext	= Black::NativeTypeContext<TResult>;
+
+	private:
+		constexpr static auto FUNCTION_HANDLER	= JniContext::STATIC_FUNCTION_HANDLER;
 
 		JNIEnv*		m_local_env		= nullptr;	// Current thread-local JNI environment.
-		jclass		m_class_ref		= nullptr;	// Reference to Java object.
-		jmethodID	m_function_id	= 0;		// Id of function.
+		jclass		m_class_ref		= nullptr;	// JNI class reference.
+		jmethodID	m_function_id	= nullptr;	// Id of function.
 	};
 
-	/// @brief	Invocation of Java static function with `void` result type. Directly used by `MemberFunction` to invoke the Java code.
-	template< typename... TNativeArguments >
-	class StaticFunctionInvocation<void, TNativeArguments...>
+	// Entry-point for JNI static function with void result.
+	template< typename... TArguments >
+	class StaticFunctionEntry< void, TArguments... >
 	{
 	public:
-		inline StaticFunctionInvocation( JNIEnv* local_env, jclass class_ref, jmethodID function_id );
+		inline StaticFunctionEntry( JNIEnv* local_env, jclass class_ref, jmethodID function_id );
 
-		/// @brief	Call the Java method on given Java class with given function arguments.
-		inline void Call( const TNativeArguments&... arguments ) const;
+		// Execute the bound function.
+		inline void Call( const TArguments&... arguments ) const;
 
 	private:
-		constexpr static auto FUNCTION_HANDLER	= Jni::Marshaling::NativeTypeTraits<void>::STATIC_FUNCTION_HANDLER;
+		// JNI environment context.
+		using JniContext	= Black::NativeTypeContext<void>;
+
+	private:
+		constexpr static auto FUNCTION_HANDLER	= JniContext::STATIC_FUNCTION_HANDLER;
 
 		JNIEnv*		m_local_env		= nullptr;	// Current thread-local JNI environment.
-		jclass		m_class_ref		= nullptr;	// Reference to Java object.
-		jmethodID	m_function_id	= 0;		// Id of function.
+		jclass		m_class_ref		= nullptr;	// JNI class reference.
+		jmethodID	m_function_id	= nullptr;	// Id of function.
 	};
+}
+}
 }
 }
