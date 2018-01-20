a711710737ab81df312a95098622ec1b4f016fb8
Frank Stein <dr.frank.stain@gmail.com>
2018-01-20 11:00:57 +0300

    More proper name for class.

--- include/utils/JniNativeBindingTable.h
+++ include/utils/JniNativeBindingTable.h
@@ -23,7 +23,7 @@ namespace Traits
 
 
 	// Native function handlers table for JNI class.
-	struct NativeBindingTable final
+	struct JniNativeBindingTable final
 	{
 		const char*										class_name;	// JNI class path.
 		std::initializer_list<Traits::NativeFunction>	natives;	// List of native handlers.
