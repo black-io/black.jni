7233300a31bafd8d3f1074e229f0b2f3a993dac0
Frank Stein <dr.frank.stain@gmail.com>
2018-01-29 00:40:24 +0300

    New active headers.

--- include/black.jni.h
+++ include/black.jni.h
@@ -77,7 +77,7 @@ inline namespace Jni
 #include "handles/JniStaticFunction.inl"
 #include "handles/StaticFunctionEntry.inl"
 #include "handles/JniMemberFunction.inl"
-//#include "handles/MemberFunctionEntry.inl"
+#include "handles/MemberFunctionEntry.inl"
 //#include "handles/JniStaticField.inl"
 //#include "handles/JniMemberField.inl"
 
