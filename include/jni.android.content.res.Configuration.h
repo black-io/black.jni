bc8f1c42bf8903b4c05b33f53e5e59766a0c4ce7
Frank Stein <dr.frank.stain@gmail.com>
2018-02-12 10:19:09 +0300

    Stub for Configuration handle.

---
+++ include/jni.android.content.res.Configuration.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// General dependency.
+#include <black.jni.h>
+#include <jni.java.util.Locale.h>
+#include <jni.android.os.LocaleList.h>
+#include <android/configuration.h>
+
+
+// Implementation.
+#include "jni/Configuration.const.h"
+#include "jni/Configuration.struct.h"
+#include "jni/Configuration.h"
