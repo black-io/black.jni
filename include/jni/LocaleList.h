55d3ba9957f1d116741b39f789ee77be7a2fb062
Frank Stein <dr.frank.stain@gmail.com>
2018-02-13 10:02:15 +0300

    LocaleList handle implementation.

--- include/jni/LocaleList.h
+++ include/jni/LocaleList.h
@@ -1,61 +1,75 @@
-// Copyright since 2016 : Evgenii Shatunov (github.com/FrankStain/jnipp)
-// Apache 2.0 License
 #pragma once
-
-#include <jnipp/jnipp.h>
-#include "LocaleList.h"
 
 
 namespace Jni
 {
-namespace Android
+inline namespace Android
 {
-	/// @brief	Handle to `android.os.LocaleList` object.
-	class LocaleList : public Object
+inline namespace Os
+{
+	// Handle for `java.util.Locale` objects.
+	class LocaleList final : Black::JniObject
 	{
 	public:
-		/// @brief	Class path.
-		using ClassPath = Jni::StaticString<'a', 'n', 'd', 'r', 'o', 'i', 'd', '/', 'o', 's', '/', 'L', 'o', 'c', 'a', 'l', 'e', 'L', 'i', 's', 't'>;
+		// Class path.
+		using ClassPath = Black::StaticString<'a', 'n', 'd', 'r', 'o', 'i', 'd', '/', 'o', 's', '/', 'L', 'o', 'c', 'a', 'l', 'e', 'L', 'i', 's', 't'>;
 
-
+	// Construction and assignation.
+	public:
 		LocaleList() = default;
-		LocaleList( jobject object_ref ) : Object( object_ref ) {};
-		LocaleList( const LocaleList& other ) : Object( other ) {};
-		LocaleList( LocaleList&& other ) : Object( other ) {};
+		LocaleList( jobject object_ref ) : Black::JniObject{ object_ref } {};
+		LocaleList( const LocaleList& other ) : Black::JniObject( other ) {};
+		LocaleList( LocaleList&& other ) : Black::JniObject( std::move( other ) ) {};
 
 
-		/// @brief	Call of `android.os.LocaleList.isEmpty` function.
+		const LocaleList& operator = ( jobject object_ref )			{ Black::JniObject::operator=( object_ref ); return *this; };
+		const LocaleList& operator = ( const LocaleList& other )	{ Black::JniObject::operator=( other ); return *this; };
+		const LocaleList& operator = ( LocaleList&& other )			{ Black::JniObject::operator=( std::move( other ) ); return *this; };
+
+	// Public interface.
+	public:
+		// Call of `android.os.LocaleList.isEmpty` function.
 		inline const bool IsEmpty() const							{ return m_handles->is_empty.Call( *this ); };
 
-		/// @brief	Call of `android.os.LocaleList.size` function.
+		// Call of `android.os.LocaleList.size` function.
 		inline const int32_t GetSize() const						{ return m_handles->get_size.Call( *this ); };
 
-		/// @brief	Call of `android.os.LocaleList.get` function.
+		// Call of `android.os.LocaleList.get` function.
 		inline Locale GetLocale( const int32_t index ) const		{ return m_handles->get_locale.Call( *this, index ); };
 
-
-		const LocaleList& operator = ( jobject object_ref )			{ Object::operator=( object_ref ); return *this; };
-		const LocaleList& operator = ( const LocaleList& other )	{ Object::operator=( other ); return *this; };
-		const LocaleList& operator = ( LocaleList&& other )			{ Object::operator=( other ); return *this; };
-
+	// Private state.
 	private:
-		struct LocaleListHandles
+		struct LocaleListState final
 		{
-			Class	jni_class	= { ClassPath::GetString() };
+			Black::JniClass	class_handle{ ClassPath::GetData() };
 
-			MemberFunction<bool>			is_empty	= { jni_class, "isEmpty" };
-			MemberFunction<int32_t>			get_size	= { jni_class, "size" };
-			MemberFunction<Locale, int32_t>	get_locale	= { jni_class, "get" };
+			Black::JniMemberFunction<bool>				is_empty	{ class_handle, "isEmpty" };
+			Black::JniMemberFunction<int32_t>			get_size	{ class_handle, "size" };
+			Black::JniMemberFunction<Locale, int32_t>	get_locale	{ class_handle, "get" };
 		};
 
-		CachedHandles<LocaleListHandles>	m_handles;	// Temporally cached and shared handles for object.
+		SharedState<LocaleListState>	m_handles; // Shared JNI handles.
 	};
 }
+}
+}
 
-namespace Marshaling
+
+namespace Black
 {
-	/// @brief	Traits specification for native `Jni::Android::LocaleList` type.
+inline namespace Jni
+{
+inline namespace Marshaling
+{
+namespace Traits
+{
+	// JNI context specification for handles to `java.util.Locale` class.
 	template<>
-	struct NativeTypeTraits<Jni::Android::LocaleList> : ObjectTypeTraits<Jni::Android::LocaleList> {};
+	struct NativeContext<::Jni::Android::Os::LocaleList> : public Black::NativeObjectContext<::Jni::Android::Os::LocaleList>
+	{
+
+	};
+}
+}
 }
 }
