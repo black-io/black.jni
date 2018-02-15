b3a288d9d169866386ded7a0aa3832965597f491
Frank Stein <dr.frank.stain@gmail.com>
2018-02-15 23:32:37 +0300

    Handle for Android keypad event.

--- include/jni/KeyEvent.h
+++ include/jni/KeyEvent.h
@@ -1,79 +1,91 @@
-// Copyright since 2016 : Evgenii Shatunov (github.com/FrankStain/jnipp)
-// Apache 2.0 License
 #pragma once
-
-#include <jnipp/jnipp.h>
-#include <android/input.h>
-#include "InputDevice.const.h"
-#include "KeyEvent.struct.h"
 
 
 namespace Jni
 {
-namespace Android
+inline namespace Android
 {
-	/// @brief	Handle to `android.view.KeyEvent` object.
-	class KeyEvent : public Object
+inline namespace View
+{
+	// Handle for `android.view.KeyEvent` objects.
+	class KeyEvent final : public Black::JniObject
 	{
 	public:
-		/// @brief	Class path.
-		using ClassPath = Jni::StaticString<'a', 'n', 'd', 'r', 'o', 'i', 'd', '/', 'v', 'i', 'e', 'w', '/', 'K', 'e', 'y', 'E', 'v', 'e', 'n', 't'>;
+		// Class path.
+		using ClassPath = Black::StaticString<'a', 'n', 'd', 'r', 'o', 'i', 'd', '/', 'v', 'i', 'e', 'w', '/', 'K', 'e', 'y', 'E', 'v', 'e', 'n', 't'>;
 
-
+	// Construction and assignation.
+	public:
 		KeyEvent() = default;
-		KeyEvent( jobject object_ref ) : Object( object_ref ) {};
-		KeyEvent( const KeyEvent& other ) : Object( other ) {};
-		KeyEvent( KeyEvent&& other ) : Object( other ) {};
+		KeyEvent( jobject object_ref ) : Black::JniObject{ object_ref } {};
+		KeyEvent( const KeyEvent& other ) : Black::JniObject( other ) {};
+		KeyEvent( KeyEvent&& other ) : Black::JniObject( std::move( other ) ) {};
 
 
-		/// @brief	Call of `android.view.KeyEvent.getCharacters` function.
+		const KeyEvent& operator = ( jobject object_ref )		{ Black::JniObject::operator=( object_ref ); return *this; };
+		const KeyEvent& operator = ( const KeyEvent& other )	{ Black::JniObject::operator=( other ); return *this; };
+		const KeyEvent& operator = ( KeyEvent&& other )			{ Black::JniObject::operator=( std::move( other ) ); return *this; };
+
+	// Public interface.
+	public:
+		// Call of `android.view.KeyEvent.getCharacters` function.
 		inline std::string GetChars() const						{ return m_handles->get_chars.Call( *this ); };
 
-		/// @brief	Call of `android.view.KeyEvent.getDisplayLabel` function.
+		// Call of `android.view.KeyEvent.getDisplayLabel` function.
 		inline char16_t GetLabel() const						{ return m_handles->get_label.Call( *this ); };
 
-		/// @brief	Call of `android.view.KeyEvent.getDownTime` function.
-		inline std::chrono::milliseconds GetDownTIme() const	{ return std::chrono::milliseconds{ m_handles->get_down_time.Call( *this ) }; };
+		// Call of `android.view.KeyEvent.getDownTime` function.
+		inline std::chrono::milliseconds GetDownTime() const	{ return std::chrono::milliseconds{ m_handles->get_down_time.Call( *this ) }; };
 
-		/// @brief	Call of `android.view.KeyEvent.getEventTime` function.
+		// Call of `android.view.KeyEvent.getEventTime` function.
 		inline std::chrono::milliseconds GetEventTime() const	{ return std::chrono::milliseconds{ m_handles->get_event_time.Call( *this ) }; };
 
-		/// @brief	Call of `android.view.KeyEvent.getKeyCode` function.
+		// Call of `android.view.KeyEvent.getKeyCode` function.
 		inline const int32_t GetKeyCode() const					{ return m_handles->get_key_code.Call( *this ); };
 
-		/// @brief	Call of `android.view.KeyEvent.getMetaState` function.
-		inline KeyMetaState GetMetaState() const				{ return m_handles->get_meta_state.Call( *this ); };
+		// Call of `android.view.KeyEvent.getMetaState` function.
+		inline Jni::KeyMetaState GetMetaState() const			{ return m_handles->get_meta_state.Call( *this ); };
 
-		/// @brief	Call of `android.view.KeyEvent.getRepeatCount` function.
+		// Call of `android.view.KeyEvent.getRepeatCount` function.
 		inline int32_t GetScanCode() const						{ return m_handles->get_scan_code.Call( *this ); };
 
-
-		const KeyEvent& operator = ( jobject object_ref )		{ Object::operator=( object_ref ); return *this; };
-		const KeyEvent& operator = ( const KeyEvent& other )	{ Object::operator=( other ); return *this; };
-		const KeyEvent& operator = ( KeyEvent&& other )			{ Object::operator=( other ); return *this; };
-
+	// Private state.
 	private:
-		struct KeyEventHandles
+		struct KeyEventState final
 		{
-			Class	jni_class	= { ClassPath::GetString() };
+			Black::JniClass	class_handle{ ClassPath::GetData() };
 
-			MemberFunction<std::string>		get_chars		= { jni_class,	"getCharacters" };
-			MemberFunction<char16_t>		get_label		= { jni_class,	"getDisplayLabel" };
-			MemberFunction<int64_t>			get_down_time	= { jni_class,	"getDownTime" };
-			MemberFunction<int64_t>			get_event_time	= { jni_class,	"getEventTime" };
-			MemberFunction<int32_t>			get_key_code	= { jni_class,	"getKeyCode" };
-			MemberFunction<KeyMetaState>	get_meta_state	= { jni_class,	"getMetaState" };
-			MemberFunction<int32_t>			get_scan_code	= { jni_class,	"getRepeatCount" };
+			Black::JniMemberFunction<std::string>		get_chars		{ class_handle,	"getCharacters" };
+			Black::JniMemberFunction<char16_t>			get_label		{ class_handle,	"getDisplayLabel" };
+			Black::JniMemberFunction<int64_t>			get_down_time	{ class_handle,	"getDownTime" };
+			Black::JniMemberFunction<int64_t>			get_event_time	{ class_handle,	"getEventTime" };
+			Black::JniMemberFunction<int32_t>			get_key_code	{ class_handle,	"getKeyCode" };
+			Black::JniMemberFunction<Jni::KeyMetaState>	get_meta_state	{ class_handle,	"getMetaState" };
+			Black::JniMemberFunction<int32_t>			get_scan_code	{ class_handle,	"getRepeatCount" };
 		};
 
-		CachedHandles<KeyEventHandles, true>	m_handles;	// Permanently cached handles for object.
+		SharedState<KeyEventState, true>	m_handles;	// Shared JNI handles.
 	};
 }
+}
+}
 
-namespace Marshaling
+
+namespace Black
 {
-	/// @brief	Traits specification for native `Jni::Android::KeyEvent` type.
+inline namespace Jni
+{
+inline namespace Marshaling
+{
+namespace Traits
+{
+	// JNI context specification for handles to `android.view.KeyEvent` class.
 	template<>
-	struct NativeTypeTraits<Jni::Android::KeyEvent> : ObjectTypeTraits<Jni::Android::KeyEvent> {};
+	struct NativeContext<::Jni::Android::View::KeyEvent> : public Black::NativeObjectContext<::Jni::Android::View::KeyEvent>
+	{
+
+	};
+}
+}
 }
 }
