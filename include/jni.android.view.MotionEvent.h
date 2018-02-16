d28ccb82289503445cd40295c8290e5370c9d7d8
Frank Stein <dr.frank.stain@gmail.com>
2018-02-16 08:53:12 +0300

    Master-header for MotionEvent.

---
+++ include/jni.android.view.MotionEvent.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// General dependency.
+#include <black.jni.h>
+#include <android/input.h>
+
+
+// Implementation.
+#include "jni/InputDevice.const.h"
+#include "jni/MotionEvent.const.h"
+#include "jni/MotionEvent.h"
