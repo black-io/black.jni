603833fea49e93142b5fbe42e611deeed8758672
Frank Stein <dr.frank.stain@gmail.com>
2018-02-16 09:23:24 +0300

    MotionEvent handle.

--- include/jni/MotionEvent.h
+++ include/jni/MotionEvent.h
@@ -1,97 +1,109 @@
-// Copyright since 2016 : Evgenii Shatunov (github.com/FrankStain/jnipp)
-// Apache 2.0 License
 #pragma once
-
-#include <jnipp/jnipp.h>
-#include <android/input.h>
-#include "InputDevice.const.h"
-#include "MotionEvent.const.h"
 
 
 namespace Jni
 {
-namespace Android
+inline namespace Android
 {
-	/// @brief	Handle to `android.view.MotionEvent` object.
-	class MotionEvent : public Object
+inline namespace View
+{
+	// Handle for `android.view.MotionEvent` objects.
+	class MotionEvent final : public Black::JniObject
 	{
 	public:
-		/// @brief	Class path.
-		using ClassPath = Jni::StaticString<
+		// Class path.
+		using ClassPath = Black::StaticString<
 			'a', 'n', 'd', 'r', 'o', 'i', 'd', '/',
 			'v', 'i', 'e', 'w', '/',
 			'M', 'o', 't', 'i', 'o', 'n', 'E', 'v', 'e', 'n', 't'
 		>;
 
-
+	// Construction and assignation.
+	public:
 		MotionEvent() = default;
-		MotionEvent( jobject object_ref ) : Object( object_ref ) {};
-		MotionEvent( const MotionEvent& other ) : Object( other ) {};
-		MotionEvent( MotionEvent&& other ) : Object( other ) {};
+		MotionEvent( jobject object_ref ) : Black::JniObject{ object_ref } {};
+		MotionEvent( const MotionEvent& other ) : Black::JniObject( other ) {};
+		MotionEvent( MotionEvent&& other ) : Black::JniObject( std::move( other ) ) {};
 
 
-		/// @brief Get the class of input device.
-		inline const InputSourceClass GetSource() const			{ return m_handles->get_source_class.Call( *this ); };
+		const MotionEvent& operator = ( jobject object_ref )		{ Black::JniObject::operator=( object_ref ); return *this; };
+		const MotionEvent& operator = ( const MotionEvent& other )	{ Black::JniObject::operator=( other ); return *this; };
+		const MotionEvent& operator = ( MotionEvent&& other )		{ Black::JniObject::operator=( std::move( other ) ); return *this; };
 
-		/// @brief	Get the type of motion action, without pointer index information.
-		inline const MotionActionType GetActionType() const		{ return m_handles->get_action_masked.Call( *this ); };
+	// Public interface.
+	public:
+		// Get the class of input device.
+		inline const Jni::InputSourceClass GetSource() const				{ return m_handles->get_source_class.Call( *this ); };
 
-		/// @brief	Get the point index associated with action.
-		inline const int32_t GetPointIndex() const				{ return m_handles->get_action_index.Call( *this ); };
+		// Get the type of motion action, without pointer index information.
+		inline const Jni::MotionActionType GetActionType() const			{ return m_handles->get_action_masked.Call( *this ); };
 
-		/// @brief	Get the number of points in event.
-		inline const int32_t GetPointsCount() const				{ return m_handles->get_pointer_count.Call( *this ); };
+		// Get the point index associated with action.
+		inline const int32_t GetPointIndex() const							{ return m_handles->get_action_index.Call( *this ); };
 
-		/// @brief	Get the time (in ms) when the user originally pressed down to start a stream of position events.
-		inline const int64_t GetDownTime() const				{ return m_handles->get_down_time.Call( *this ); };
+		// Get the number of points in event.
+		inline const int32_t GetPointsCount() const							{ return m_handles->get_pointer_count.Call( *this ); };
 
-		/// @brief	Gets the state of all buttons that are pressed such as a mouse or stylus button.
-		inline const int32_t GetButtonState() const				{ return m_handles->get_buton_state.Call( *this ); };
+		// Get the time (in ms) when the user originally pressed down to start a stream of position events.
+		inline const int64_t GetDownTime() const							{ return m_handles->get_down_time.Call( *this ); };
+
+		// Gets the state of all buttons that are pressed such as a mouse or stylus button.
+		inline const int32_t GetButtonState() const							{ return m_handles->get_buton_state.Call( *this ); };
 
 
-		/// @brief	Get the id of point by given index.
+		// Get the id of point by given index.
 		inline const int32_t GetPointId( const int32_t index ) const		{ return m_handles->get_pointer_id.Call( *this, index ); };
 
-		/// @brief	Get the X coordinate of point.
+		// Get the X coordinate of point.
 		inline const float GetPointX( const int32_t index ) const			{ return m_handles->get_pointer_x.Call( *this, index ); };
 
-		/// @brief	Get the Y coordinate of point.
+		// Get the Y coordinate of point.
 		inline const float GetPointY( const int32_t index ) const			{ return m_handles->get_pointer_y.Call( *this, index ); };
 
-		/// @brief	Get the pressure of point.
+		// Get the pressure of point.
 		inline const float GetPointPressure( const int32_t index ) const	{ return m_handles->get_pointer_pressure.Call( *this, index ); };
 
-
-		const MotionEvent& operator = ( jobject object_ref )		{ Object::operator=( object_ref ); return *this; };
-		const MotionEvent& operator = ( const MotionEvent& other )	{ Object::operator=( other ); return *this; };
-		const MotionEvent& operator = ( MotionEvent&& other )		{ Object::operator=( other ); return *this; };
-
+	// Private state.
 	private:
-		struct MotionEventHandles
+		struct MotionEventState final
 		{
-			Class	jni_class	= { ClassPath::GetString() };
+			Black::JniClass	class_handle{ ClassPath::GetData() };
 
-			MemberFunction<int32_t>				get_pointer_count	= { jni_class, "getPointerCount" };
-			MemberFunction<InputSourceClass>	get_source_class	= { jni_class, "getSource" };
-			MemberFunction<MotionActionType>	get_action_masked	= { jni_class, "getActionMasked" };
-			MemberFunction<int32_t>				get_action_index	= { jni_class, "getActionIndex" };
-			MemberFunction<int64_t>				get_down_time		= { jni_class, "getDownTime" };
-			MemberFunction<int32_t>				get_buton_state		= { jni_class, "getButtonState" };
+			Black::JniMemberFunction<int32_t>				get_pointer_count		{ class_handle, "getPointerCount" };
+			Black::JniMemberFunction<Jni::InputSourceClass>	get_source_class		{ class_handle, "getSource" };
+			Black::JniMemberFunction<Jni::MotionActionType>	get_action_masked		{ class_handle, "getActionMasked" };
+			Black::JniMemberFunction<int32_t>				get_action_index		{ class_handle, "getActionIndex" };
+			Black::JniMemberFunction<int64_t>				get_down_time			{ class_handle, "getDownTime" };
+			Black::JniMemberFunction<int32_t>				get_buton_state			{ class_handle, "getButtonState" };
 
-			MemberFunction<int32_t, int32_t>	get_pointer_id			= { jni_class, "getPointerId" };
-			MemberFunction<float, int32_t>		get_pointer_x			= { jni_class, "getX" };
-			MemberFunction<float, int32_t>		get_pointer_y			= { jni_class, "getY" };
-			MemberFunction<float, int32_t>		get_pointer_pressure	= { jni_class, "getPressure" };
+			Black::JniMemberFunction<int32_t, int32_t>		get_pointer_id			{ class_handle, "getPointerId" };
+			Black::JniMemberFunction<float, int32_t>		get_pointer_x			{ class_handle, "getX" };
+			Black::JniMemberFunction<float, int32_t>		get_pointer_y			{ class_handle, "getY" };
+			Black::JniMemberFunction<float, int32_t>		get_pointer_pressure	{ class_handle, "getPressure" };
 		};
 
-		CachedHandles<MotionEventHandles, true>	m_handles;	// Permanently cached handles for object.
+		SharedState<MotionEventState, true>	m_handles;	// Shared JNI handles.
 	};
 }
+}
+}
 
-namespace Marshaling
+
+namespace Black
 {
-	/// @brief	Traits specification for native `Jni::Android::MotionEvent` type.
+inline namespace Jni
+{
+inline namespace Marshaling
+{
+namespace Traits
+{
+	// JNI context specification for handles to `java.util.Locale` class.
 	template<>
-	struct NativeTypeTraits<Jni::Android::MotionEvent> : ObjectTypeTraits<Jni::Android::MotionEvent> {};
+	struct NativeContext<::Jni::Android::View::MotionEvent> : public Black::NativeObjectContext<::Jni::Android::View::MotionEvent>
+	{
+
+	};
+}
+}
 }
 }
