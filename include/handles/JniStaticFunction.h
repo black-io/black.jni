d585f1eadf2350f1d834c579f3ae46b877218552
Frank Stein <dr.frank.stain@gmail.com>
2018-02-17 09:40:18 +0300

    Wrong type fixed.

--- include/handles/JniStaticFunction.h
+++ include/handles/JniStaticFunction.h
@@ -53,7 +53,7 @@ inline namespace Handles
 	// Private interface.
 	private:
 		// JNI type signature.
-		using Signature		= Black::NativeTypeSignature<TNativeType>;
+		using Signature		= Black::NativeTypeSignature<TResult>;
 
 		// Entry-point.
 		using EntryPoint	= Traits::StaticFunctionEntry<TResult, TArguments...>;
