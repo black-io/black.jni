242cf3c8c84695ecbf912842e90108e5875414da
Frank Stein <dr.frank.stain@gmail.com>
2018-02-18 15:13:37 +0300

    More verbose logging for non-release builds.

--- include/utils/logging.h
+++ include/utils/logging.h
@@ -50,45 +50,56 @@
 #endif
 
 
+#if( BLACK_RELEASE_BUILD )
+	#define BLACK_JNI_FORMAT_MESSAGE( FORMAT, ... ) const std::string message{ Black::FormatString( FORMAT, ##__VA_ARGS__ ) };
+#else
+	#define BLACK_JNI_FORMAT_MESSAGE( FORMAT, ... )																			\
+	const std::string message{ Black::FormatString(																			\
+		FORMAT " [at `{}` from '" __FILE__ "':" BLACK_STRINGIFICATION( __LINE__ ) "]",										\
+		##__VA_ARGS__, __FUNCTION__																							\
+	) }
+#endif
+
+
 #define BLACK_LOG_ASSERT( CHANNEL, FORMAT, ... )								\
 {																				\
-	const std::string message = Black::FormatString( FORMAT, ##__VA_ARGS__ );	\
+	BLACK_JNI_FORMAT_MESSAGE( FORMAT, ##__VA_ARGS__ );							\
 	__android_log_write( ANDROID_LOG_ERROR, CHANNEL, message.c_str() );			\
 }
 
 #define BLACK_LOG_CRITICAL( CHANNEL, FORMAT, ... )								\
 {																				\
-	const std::string message = Black::FormatString( FORMAT, ##__VA_ARGS__ );	\
+	BLACK_JNI_FORMAT_MESSAGE( FORMAT, ##__VA_ARGS__ );							\
 	__android_log_write( ANDROID_LOG_ERROR, CHANNEL, message.c_str() );			\
 }
 
 #define BLACK_LOG_ERROR( CHANNEL, FORMAT, ... )									\
 {																				\
-	const std::string message = Black::FormatString( FORMAT, ##__VA_ARGS__ );	\
+	BLACK_JNI_FORMAT_MESSAGE( FORMAT, ##__VA_ARGS__ );							\
 	__android_log_write( ANDROID_LOG_ERROR, CHANNEL, message.c_str() );			\
 }
 
 #define BLACK_LOG_WARNING( CHANNEL, FORMAT, ... )								\
 {																				\
-	const std::string message = Black::FormatString( FORMAT, ##__VA_ARGS__ );	\
+	BLACK_JNI_FORMAT_MESSAGE( FORMAT, ##__VA_ARGS__ );							\
 	__android_log_write( ANDROID_LOG_WARN, CHANNEL, message.c_str() );			\
 }
 
 #define BLACK_LOG_INFO( CHANNEL, FORMAT, ... )									\
 {																				\
-	const std::string message = Black::FormatString( FORMAT, ##__VA_ARGS__ );	\
+	BLACK_JNI_FORMAT_MESSAGE( FORMAT, ##__VA_ARGS__ );							\
 	__android_log_write( ANDROID_LOG_INFO, CHANNEL, message.c_str() );			\
 }
 
 #define BLACK_LOG_VERBOSE( CHANNEL, FORMAT, ... )								\
 {																				\
-	const std::string message = Black::FormatString( FORMAT, ##__VA_ARGS__ );	\
+	BLACK_JNI_FORMAT_MESSAGE( FORMAT, ##__VA_ARGS__ );							\
 	__android_log_write( ANDROID_LOG_VERBOSE, CHANNEL, message.c_str() );		\
 }
 
 #define BLACK_LOG_DEBUG( CHANNEL, FORMAT, ... )									\
 {																				\
-	const std::string message = Black::FormatString( FORMAT, ##__VA_ARGS__ );	\
+	BLACK_JNI_FORMAT_MESSAGE( FORMAT, ##__VA_ARGS__ );							\
 	__android_log_write( ANDROID_LOG_DEBUG, CHANNEL, message.c_str() );			\
 }
 
