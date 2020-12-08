b113fe017c22aedb65973f0241b4d988211e9ee9
Frank Stein <dr.frank.stain@gmail.com>
2020-12-08 18:20:33 +0300

    Stubs for library prologue and epilogue.

---
+++ include/.prologue.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+// External dependencies.
+#include <black/core.minimal.h>
+
+
+// The library namespace definition.
+#include ".bootstrap/namespace.h"
+
+// Environment configuration.
+#include ".bootstrap/configuration.h"
