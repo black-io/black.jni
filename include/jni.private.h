4cdb3e7d072f94a10ae7976491f99deff4b92584
Frank Stein <dr.frank.stain@gmail.com>
2020-12-09 23:57:26 +0300

    Virtual machine subsystem included.

--- include/jni.private.h
+++ include/jni.private.h
@@ -6,8 +6,10 @@
 
 #include "marshaling/marshaling.h"
 #include "handles/handles.h"
+#include "virtual-machine/virtual-machine.h"
 
 // Deferred implementations.
+#include "virtual-machine/virtual-machine.inl"
 #include "handles/handles.inl"
 #include "marshaling/marshaling.inl"
 
