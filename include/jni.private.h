aa569c02140e7e6fd1cd2b31d96af3f6bbd99a05
Frank Stein <dr.frank.stain@gmail.com>
2020-12-09 18:17:23 +0300

    Handles subsystem included.

--- include/jni.private.h
+++ include/jni.private.h
@@ -5,8 +5,10 @@
 #include ".prologue.h"
 
 #include "marshaling/marshaling.h"
+#include "handles/handles.h"
 
 // Deferred implementations.
+#include "handles/handles.inl"
 #include "marshaling/marshaling.inl"
 
 // The epilogue of library.
