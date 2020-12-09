53618f83129a38315de865a23d967787e40bfa3b
Frank Stein <dr.frank.stain@gmail.com>
2020-12-09 18:15:50 +0300

    Namespace definition for handles subsystem.

---
+++ include/handles/namespace.h
@@ -0,0 +1,18 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace Jni
+{
+// Java object handles subsystem.
+inline namespace Handles
+{
+// Internal stuff for JNI handles.
+namespace Internal
+{
+
+}
+}
+}
+}
