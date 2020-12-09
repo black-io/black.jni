38d7703aff13ac27dfdd037597ae4a6caef46eb1
Frank Stein <dr.frank.stain@gmail.com>
2020-12-09 22:05:08 +0300

    Master-header for JNI handles subsystem.

---
+++ include/handles/handles.h
@@ -0,0 +1,25 @@
+#pragma once
+
+
+// Namespace declaration.
+#include "namespace.h"
+
+
+// JNI handles.
+#include "JniClass.h"
+#include "marshaling/NativeTypeContext.JniClass.h"
+
+#include "internal/SharedStateStorage.h"
+#include "internal/SharedState.h"
+#include "JniObject.h"
+#include "marshaling/NativeTypeContext.JniObject.h"
+#include "marshaling/NativeObjectContext.h"
+
+#include "JniMemberField.h"
+#include "JniStaticField.h"
+
+#include "MemberFunctionEntry.h"
+#include "JniMemberFunction.h"
+
+#include "StaticFunctionEntry.h"
+#include "JniStaticFunction.h"
---
+++ include/handles/handles.inl
@@ -0,0 +1,14 @@
+#pragma once
+
+
+#include "handles/JniStaticFunction.inl"
+#include "handles/StaticFunctionEntry.inl"
+#include "handles/JniMemberFunction.inl"
+#include "handles/MemberFunctionEntry.inl"
+#include "handles/JniStaticField.inl"
+#include "handles/JniMemberField.inl"
+
+#include "handles/JniObject.inl"
+#include "internal/SharedState.inl"
+#include "internal/SharedStateStorage.inl"
+#include "handles/JniClass.inl"
