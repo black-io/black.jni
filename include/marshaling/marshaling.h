99bd1ad9df24405b72091cde5e55109cbc101d6f
Frank Stein <dr.frank.stain@gmail.com>
2020-12-09 13:59:35 +0300

    Master-header and implementation header for marshalibg.

---
+++ include/marshaling/marshaling.h
@@ -0,0 +1,30 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace Jni
+{
+// The marshaling subsystem for JNI library.
+inline namespace Marshaling
+{
+// Internal stuff for JNI marshaling.
+namespace Internal
+{
+
+}
+}
+}
+}
+
+
+#include "JniTypeSignature.h"
+#include "JniTypeContext.h"
+#include "JniArrayTranslation.h"
+#include "NativeTypeContext.h"
+
+#include "GetJniLocalFrameSize.h"
+
+#include "NativeEnumContext.h"
+#include "NativeBitfieldContext.h"
+#include "NativeTypeContext.EnumFlags.h"
---
+++ include/marshaling/marshaling.inl
@@ -0,0 +1,7 @@
+#pragma once
+
+
+#include "NativeTypeContext.FromJni.inl"
+#include "NativeTypeContext.ToJni.inl"
+#include "JniArrayTranslation.FromJni.inl"
+#include "JniArrayTranslation.ToJni.inl"
