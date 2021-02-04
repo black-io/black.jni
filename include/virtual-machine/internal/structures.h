d027e17392f14fe74d5c3439e0d12be3c464d24c
Frank Stein <dr.frank.stain@gmail.com>
2021-02-04 18:30:27 +0300

    File renamed to fit its meaning.

--- include/virtual-machine/internal/structures.h
+++ include/virtual-machine/internal/structures.h
@@ -7,12 +7,15 @@ inline namespace Jni
 {
 inline namespace VirtualMachine
 {
+namespace Internal
+{
 	// Native function handlers table for JNI class.
 	struct NativeBindingTable final
 	{
-		const char*										class_name;	// JNI class path.
-		std::initializer_list<Internal::NativeFunction>	natives;	// List of native handlers.
+		const char*								class_name;	// JNI class path.
+		std::initializer_list<JNINativeMethod>	natives;	// List of native handlers.
 	};
+}
 }
 }
 }
