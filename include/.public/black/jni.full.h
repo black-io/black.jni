3523ca1f164c053f0a3a0933738a3c4fe0eb803f
Frank Stein <dr.frank.stain@gmail.com>
2021-02-04 18:34:53 +0300

    Library header with all subsystems included.

---
+++ include/.public/black/jni.full.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+// Declaration for JNI logging to restore the macros.
+#define BLACK_JNI_RESTORE_ISOLATED_LOGS
+
+// Just include the private master-header.
+#include "../../jni.private.h"
