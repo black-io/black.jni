20fcf5c799a1409468c2e5088d3a54e36af8dbca
Frank Stein <dr.frank.stain@gmail.com>
2021-02-06 08:42:53 +0300

    Public header for startup subsystem.

---
+++ include/.public/black/jni/startup.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+// The prologue of library.
+#include "../../../.prologue.h"
+
+// JNI startup subsystem.
+#include "../../../startup/startup.h"
+
+// The epilogue of library.
+#include "../../../.epilogue.h"
