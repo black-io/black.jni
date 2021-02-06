895c90c6e33bf5c7dfe216741fc9affb12db4b7a
Frank Stein <dr.frank.stain@gmail.com>
2021-02-06 09:10:34 +0300

    JNI connection moved into global section.

--- include/.global/jni-connection/internal/structures.h
+++ include/.global/jni-connection/internal/structures.h
@@ -12,8 +12,8 @@ namespace Internal
 	// Native function handlers table for JNI class.
 	struct NativeBindingTable final
 	{
-		const char*								class_name;	// JNI class path.
-		std::initializer_list<JNINativeMethod>	natives;	// List of native handlers.
+		const char*									class_name;	// JNI class path.
+		std::initializer_list<::JNINativeMethod>	natives;	// List of native handlers.
 	};
 }
 }
