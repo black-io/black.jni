92016430312008d6b46ba924b0bed7cba4b66f07
Frank Stein <dr.frank.stain@gmail.com>
2021-02-08 16:34:39 +0300

    Some forward declarations inside of subsystem.

---
+++ include/handles/objects/forwards.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace Jni
+{
+inline namespace Handles
+{
+inline namespace Objects
+{
+	// Forward declaration to use the object handle in interface of class handle.
+	class JniObject;
+}
+}
+}
+}
