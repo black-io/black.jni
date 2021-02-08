8ca91b88ea70eaf215ed350530e6b5cacd67ca7e
Frank Stein <dr.frank.stain@gmail.com>
2021-02-08 23:28:25 +0300

    Static function handle refactored.

--- include/handles/functions/JniStaticFunction.h
+++ include/handles/functions/JniStaticFunction.h
@@ -7,19 +7,30 @@ inline namespace Jni
 {
 inline namespace Handles
 {
+inline namespace Functions
+{
+	/**
+		@brief	JNI static function handle.
+
+		This template implements the functionality of calling the static functions of some class.
+		The function template should be instantiated for the valid function signature, where only native C++ types present.
+		This handle automatically converts all given arguments on call.
+		The result of Java call will be also automatically converted back to native type, when required.
+
+		@tparam	TFunction Valid function signature.
+	*/
+	template< typename TFunction >
+	class JniStaticFunction;
+
 	// Static function handle for JNI classes.
 	template< typename TResult, typename... TArguments >
-	class JniStaticFunction final
+	class JniStaticFunction<TResult (TArguments...)> final
 	{
-		friend class Black::Jni::VirtualMachine::JniConnection;		// Grant access to function calls.
-		friend class Black::Jni::VirtualMachine::JniEnvironment;	// Grant access to function calls.
-
 	// Construction and assignment.
 	public:
-		JniStaticFunction()									= default;
-
-		JniStaticFunction( const JniStaticFunction& other )	= default;
-		JniStaticFunction( JniStaticFunction&& other )		= default;
+		JniStaticFunction()								= default;
+		JniStaticFunction( const JniStaticFunction& )	= default;
+		JniStaticFunction( JniStaticFunction&& )		= default;
 
 		JniStaticFunction( std::string_view class_name, std::string_view function_name );
 		JniStaticFunction( const Black::JniClass& class_handle, std::string_view function_name );
@@ -28,8 +39,8 @@ inline namespace Handles
 		JniStaticFunction( const Black::JniClass& class_handle, std::string_view function_name, Black::IgnoreFailure );
 
 
-		inline JniStaticFunction& operator = ( const JniStaticFunction& other )	= default;
-		inline JniStaticFunction& operator = ( JniStaticFunction&& other )		= default;
+		inline JniStaticFunction& operator = ( const JniStaticFunction& )	= default;
+		inline JniStaticFunction& operator = ( JniStaticFunction&& )		= default;
 
 	// Public interface.
 	public:
@@ -47,28 +58,35 @@ inline namespace Handles
 		inline std::string_view GetSignature() const	{ return { Signature::GetData(), Signature::GetLength() }; };
 
 
-		inline explicit operator const bool () const	{ return IsValid(); };
 		inline jmethodID operator * () const			{ return GetFunctionId(); };
+
+		inline explicit operator const bool () const	{ return IsValid(); };
+		inline const bool operator ! () const			{ return !IsValid(); };
+
+	// Private inner types.
+	private:
+		// JNI type signature.
+		using Signature = Black::NativeFunctionSignature<TResult, TArguments...>;
+
+		// Entry-point.
+		using EntryPoint = Internal::StaticFunctionEntry<TResult, TArguments...>;
 
 	// Private interface.
 	private:
-		// JNI type signature.
-		using Signature		= Black::NativeFunctionSignature<TResult, TArguments...>;
-
-		// Entry-point.
-		using EntryPoint	= Internal::StaticFunctionEntry<TResult, TArguments...>;
-
-
 		// Call the function using given arguments and local JNI environment.
 		inline TResult Call( JNIEnv* local_env, const TArguments&... arguments ) const;
 
+	// Private static fields.
+	private:
+		// Logging channel.
+		static constexpr const char* LOG_CHANNEL = "Black/Jni/StaticFunction";
+
 	// Private state.
 	private:
-		static constexpr const char*	LOG_CHANNEL	= "Black/Jni/StaticFunction";
-
-		Black::JniClass		m_class_handle;				// Handle to class owning the static method.
-		jmethodID			m_function_id	= nullptr;	// Id of Java object method.
+		Black::JniClass	m_class_handle;				// Handle to class owning the static method.
+		jmethodID		m_function_id	= nullptr;	// Id of Java object method.
 	};
+}
 }
 }
 }
--- include/handles/functions/JniStaticFunction.inl
+++ include/handles/functions/JniStaticFunction.inl
@@ -7,29 +7,31 @@ inline namespace Jni
 {
 inline namespace Handles
 {
+inline namespace Functions
+{
 	template< typename TResult, typename... TArguments >
-	JniStaticFunction<TResult, TArguments...>::JniStaticFunction( std::string_view class_name, std::string_view function_name )
+	JniStaticFunction<TResult (TArguments...)>::JniStaticFunction( std::string_view class_name, std::string_view function_name )
 		: JniStaticFunction{ Black::JniClass{ class_name }, function_name, Black::IGNORE_FALURE }
 	{
 		ENSURES( IsValid() );
 	}
 
 	template< typename TResult, typename... TArguments >
-	JniStaticFunction<TResult, TArguments...>::JniStaticFunction( const Black::JniClass& class_handle, std::string_view function_name )
+	JniStaticFunction<TResult (TArguments...)>::JniStaticFunction( const Black::JniClass& class_handle, std::string_view function_name )
 		: JniStaticFunction{ class_handle, function_name, Black::IGNORE_FALURE }
 	{
 		ENSURES( IsValid() );
 	}
 
 	template< typename TResult, typename... TArguments >
-	JniStaticFunction<TResult, TArguments...>::JniStaticFunction( std::string_view class_name, std::string_view function_name, Black::IgnoreFailure )
+	JniStaticFunction<TResult (TArguments...)>::JniStaticFunction( std::string_view class_name, std::string_view function_name, Black::IgnoreFailure )
 		: JniStaticFunction{ Black::JniClass{ class_name }, function_name, Black::IGNORE_FALURE }
 	{
 
 	}
 
 	template< typename TResult, typename... TArguments >
-	JniStaticFunction<TResult, TArguments...>::JniStaticFunction( const Black::JniClass& class_handle, std::string_view function_name, Black::IgnoreFailure )
+	JniStaticFunction<TResult (TArguments...)>::JniStaticFunction( const Black::JniClass& class_handle, std::string_view function_name, Black::IgnoreFailure )
 		: m_class_handle{ class_handle }
 	{
 		CRETD( !Black::JniConnection::IsValid(), , LOG_CHANNEL, "{}:{} - Attempt to use invalid JNI connection.", __func__, __LINE__ );
@@ -50,16 +52,17 @@ inline namespace Handles
 	}
 
 	template< typename TResult, typename... TArguments >
-	inline TResult JniStaticFunction<TResult, TArguments...>::Call( const TArguments&... arguments ) const
+	inline TResult JniStaticFunction<TResult (TArguments...)>::Call( const TArguments&... arguments ) const
 	{
 		return Call( Black::JniConnection::GetLocalEnvironment(), arguments... );
 	}
 
 	template< typename TResult, typename... TArguments >
-	inline TResult JniStaticFunction<TResult, TArguments...>::Call( JNIEnv* local_env, const TArguments&... arguments ) const
+	inline TResult JniStaticFunction<TResult (TArguments...)>::Call( JNIEnv* local_env, const TArguments&... arguments ) const
 	{
 		return EntryPoint{ local_env, *m_class_handle, m_function_id }.Call( arguments... );
 	}
+}
 }
 }
 }
