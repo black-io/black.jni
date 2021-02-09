9a61e73761db2ec21974f9006360faf43756fa3f
Frank Stein <dr.frank.stain@gmail.com>
2021-02-09 19:04:27 +0300

    Class registry refactored.

--- include/handles/internal/ClassRegistry.h
+++ include/handles/internal/ClassRegistry.h
@@ -5,45 +5,81 @@ namespace Black
 {
 inline namespace Jni
 {
-inline namespace VirtualMachine
+inline namespace Handles
 {
 namespace Internal
 {
 	/**
-		@brief	Storage for shared JNI class instances.
+		@brief	Class registry.
+
+		This registry manages the shared `jclass` references. It is crucial for Java side and C++ performance to share the global class references.
 	*/
-	class SharedClassStorage final : private Black::NonTransferable
+	class ClassRegistry final : private Black::NonTransferable
 	{
-	// Public interface.
+	// Friendship declarations.
 	public:
-		// Init the storage.
-		const bool Initialize();
+		// Grant access to initialization and finalization.
+		friend class Black::JniConnection;
 
-		// Finalize.
-		const bool Finalize();
-
-
+	// Public static interface.
+	public:
 		// Get the class reference for JNI object by given JNI reference.
-		std::shared_ptr<_jclass> GetClassReference( jobject object_ref );
+		static std::shared_ptr<_jclass> GetClassReference( jobject object_ref );
 
 		// Get the shared global reference for JNI class using another reference.
-		std::shared_ptr<_jclass> GetClassReference( jclass class_ref );
+		static std::shared_ptr<_jclass> GetClassReference( jclass class_ref );
 
 		// Get the class reference by its name.
-		std::shared_ptr<_jclass> GetClassReference( std::string_view class_name );
+		static std::shared_ptr<_jclass> GetClassReference( std::string_view class_name );
+
+		// Get the class reference by its name.
+		static jclass GetLocalClassReference( std::string_view class_name );
+
+
+		// Call the `java.lang.Class.getName()`.
+		static std::string GetClassName( Black::NotNull<jclass> class_ref );
+
+		// Call the `java.lang.Class.getSimpleName()`.
+		static std::string GetSimpleClassName( Black::NotNull<jclass> class_ref );
+
+		// Call the `java.lang.Class.getCanonicalName()`.
+		static std::string GetCanonicalClassName( Black::NotNull<jclass> class_ref );
+
+		// Call the `java.lang.Class.getSuperClass()`.
+		static JniClass GetParentClass( Black::NotNull<jclass> class_ref );
 
 
 		// Get the synchronization mutex for storage.
-		inline const Black::Mutex& GetMutex() const		{ return m_latch; };
+		static inline const Black::Mutex& GetMutex() { return GetInstance().m_lock; };
 
-	// Private interface.
+	// Private inner types.
 	private:
+		// Stored global `jclass` references.
+		using SharedClassMap = std::unordered_map<std::string, std::weak_ptr<_jclass>>;
+
+	// Private static interface.
+	private:
+		// Get the global instance of connection.
+		static ClassRegistry& GetInstance();
+
+
 		// Shared class deletion routine.
 		static void DeleteSharedClass( jclass value );
 
+	// Private interface.
+	private:
+		// Initialize the registry.
+		const bool Initialize();
+
+		// Perform the finalization.
+		const bool Finalize();
+
 
 		// Capture the global handle to JNI class loader.
-		const bool CaptureClassLoader();
+		const bool AcquireClassLoader();
+
+		// Acquire the helper functions for JNI class handle.
+		const bool AcquireClassInterface();
 
 
 		// Create shared global reference to JNI class from local one.
@@ -52,18 +88,29 @@ namespace Internal
 		// Load the JNI class by given class name.
 		std::shared_ptr<_jclass> LoadClass( std::string_view class_name );
 
+		// Load the JNI class by given class name.
+		jclass LoadClass( std::string_view class_name, JNIEnv* local_env );
+
+		// Returns local reference. Perform the loading of class from main JNI thread.
+		jclass LoadClassFromMainThread( std::string_view class_name, JNIEnv* local_env );
+
+		// Returns local reference. Perform the loading of class by means of class loader instance.
+		jclass LoadClassUsingClassLoader( std::string_view class_name, JNIEnv* local_env );
+
 	// Private state.
 	private:
-		using Storage = std::unordered_map<std::string, std::weak_ptr<_jclass>>;
-
-		Black::CriticalSection	m_latch;
-		Storage					m_storage;
+		Black::SpinLock	m_lock;				// Storage synchronization lock.
+		SharedClassMap	m_class_storage;	// Storage for referenced classes.
 
 	// Persistently cached handles.
 	private:
-		Black::JniObject										m_class_loader;		// Instance of `java.lang.ClassLoader`.
+		Black::JniMemberFunction<Black::JniClass ()>	m_get_super_class;		// `java.lang.Class java.lang.Class.getSuperClass()`
+		Black::JniMemberFunction<std::string ()>		m_get_canonical_name;	// `java.lang.String java.lang.Class.getCanonicalName()`
+		Black::JniMemberFunction<std::string ()>		m_get_name;				// `java.lang.String java.lang.Class.getName()`
+		Black::JniMemberFunction<std::string ()>		m_get_simple_name;		// `java.lang.String java.lang.Class.getSimpleName()`
 
-		Black::JniMemberFunction<Black::JniClass, std::string>	m_load_class_func;	// `java.lang.Class java.lang.ClassLoader.loadClass( java.lang.String )`
+		Black::JniObject											m_class_loader;	// Instance of `java.lang.ClassLoader`.
+		Black::JniMemberFunction<Black::JniClass ( std::string )>	m_load_class;	// `java.lang.Class java.lang.ClassLoader.loadClass( java.lang.String )`
 	};
 }
 }
--- source/handles/internal/ClassRegistry.cpp
+++ source/handles/internal/ClassRegistry.cpp
@@ -1,4 +1,5 @@
-#include <jni.private.h>
+#include <black/jni/handles.h>
+
 #include <black/jni/java/lang/Thread.h>
 #include <black/jni/java/lang/ClassLoader.h>
 
@@ -7,33 +8,17 @@ namespace Black
 {
 inline namespace Jni
 {
-inline namespace VirtualMachine
+inline namespace Handles
 {
 namespace Internal
 {
 namespace
 {
-	static constexpr const char* LOG_CHANNEL = "Black/Jni/ClassStorage";
+	static constexpr const char* LOG_CHANNEL = "Black/Jni/ClassRegistry";
 }
 
 
-	const bool SharedClassStorage::Initialize()
-	{
-		CRETE( !CaptureClassLoader(), false, LOG_CHANNEL, "Failed to capture JNI class loader." );
-
-		return true;
-	}
-
-	const bool SharedClassStorage::Finalize()
-	{
-		m_load_class_func			= {};
-		m_class_loader				= {};
-
-		EXPECTS_DEBUG( m_storage.empty() );
-		return true;
-	}
-
-	std::shared_ptr<_jclass> SharedClassStorage::GetClassReference( jobject object_ref )
+	std::shared_ptr<_jclass> ClassRegistry::GetClassReference( jobject object_ref )
 	{
 		CRETD( object_ref == nullptr, {}, LOG_CHANNEL, "Attempt to get JNI class via null object." );
 
@@ -41,32 +26,86 @@ namespace
 		jclass class_ref	= local_env->GetObjectClass( object_ref );
 		CRETW( class_ref == nullptr, {}, LOG_CHANNEL, "Unable to get Java class for object." );
 
-		return MakeGlobalRef( class_ref, local_env );
+		return GetInstance().MakeGlobalRef( class_ref, local_env );
 	}
 
-	std::shared_ptr<_jclass> SharedClassStorage::GetClassReference( jclass class_ref )
+	std::shared_ptr<_jclass> ClassRegistry::GetClassReference( jclass class_ref )
 	{
 		CRETD( class_ref == nullptr, {}, LOG_CHANNEL, "Attempt to get reference for null class." );
 
-		return MakeGlobalRef( class_ref, Black::JniConnection::GetLocalEnvironment() );
+		return GetInstance().MakeGlobalRef( class_ref, Black::JniConnection::GetLocalEnvironment() );
 	}
 
-	std::shared_ptr<_jclass> SharedClassStorage::GetClassReference( std::string_view class_name )
+	std::shared_ptr<_jclass> ClassRegistry::GetClassReference( std::string_view class_name )
 	{
 		CRETD( class_name.empty(), {}, LOG_CHANNEL, "Attempt to get class reference using null class name." );
 
-		Black::MutexLock lock{ m_latch };
-		auto& weak_class_ref = m_storage[ std::string{ class_name } ];
+		ClassRegistry& registry = GetInstance();
+
+		Black::MutexLock lock{ registry.m_lock };
+		auto& weak_class_ref = registry.m_class_storage[ std::string{ class_name } ];
 		CRET( !weak_class_ref.expired(), weak_class_ref.lock() );
 
 		// If shared class already lost or never been found, ask JNI to lookup it.
-		auto shared_class_ref	= LoadClass( class_name );
+		auto shared_class_ref	= registry.LoadClass( class_name );
 		weak_class_ref			= shared_class_ref;
 
 		return shared_class_ref;
 	}
 
-	void SharedClassStorage::DeleteSharedClass( jclass value )
+	jclass ClassRegistry::GetLocalClassReference( std::string_view class_name )
+	{
+		CRETD( class_name.empty(), {}, LOG_CHANNEL, "Attempt to get class reference using null class name." );
+
+		ClassRegistry& registry	= GetInstance();
+		::JNIEnv* local_env		= Black::JniConnection::GetLocalEnvironment();
+
+		Black::MutexLock lock{ registry.m_lock };
+		auto& weak_class_ref = registry.m_class_storage[ std::string{ class_name } ];
+		CRET( weak_class_ref.expired(), registry.LoadClass( class_name, local_env ) );
+
+		auto shared_class_ref = weak_class_ref.lock();
+		return static_cast<jclass>( local_env->NewLocalRef( shared_class_ref.get() ) );
+	}
+
+	std::string ClassRegistry::GetClassName( Black::NotNull<jclass> class_ref )
+	{
+		EXPECTS_DEBUG( Black::JniConnection::IsValid() );
+
+		std::string name{ GetInstance().m_get_name.Call( class_ref ) };
+
+		std::transform( name.begin(), name.end(), name.begin(), []( char& ch ){ return ( ch == '.' )? '/' : ch; } );
+		return name;
+	}
+
+	std::string ClassRegistry::GetSimpleClassName( Black::NotNull<jclass> class_ref )
+	{
+		EXPECTS_DEBUG( Black::JniConnection::IsValid() );
+
+		return GetInstance().m_get_simple_name.Call( class_ref );
+	}
+
+	std::string ClassRegistry::GetCanonicalClassName( Black::NotNull<jclass> class_ref )
+	{
+		EXPECTS_DEBUG( Black::JniConnection::IsValid() );
+
+		return GetInstance().m_get_canonical_name.Call( class_ref );
+	}
+
+	JniClass ClassRegistry::GetParentClass( Black::NotNull<jclass> class_ref )
+	{
+		EXPECTS_DEBUG( Black::JniConnection::IsValid() );
+
+		return GetInstance().m_get_super_class.Call( class_ref );
+	}
+
+	ClassRegistry& ClassRegistry::GetInstance()
+	{
+		static ClassRegistry class_registry{};
+		return class_registry;
+	}
+
+	void ClassRegistry::DeleteSharedClass( jclass value )
 	{
 		CRETD( !Black::JniConnection::IsValid(), , LOG_CHANNEL, "{}:{} - Attempt to use invalid JNI connection.", __func__, __LINE__ );
 		CRET( value == nullptr );
@@ -74,25 +113,47 @@ namespace
 		Black::JniConnection::GetLocalEnvironment()->DeleteGlobalRef( value );
 	}
 
-	const bool SharedClassStorage::CaptureClassLoader()
+	const bool ClassRegistry::Initialize()
 	{
-		const Black::JniClass loader_class{ "java/lang/ClassLoader" };
+		CRETE( !AcquireClassLoader(), false, LOG_CHANNEL, "Failed to acquire JNI class loader." );
+		CRETE( !AcquireClassInterface(), false, LOG_CHANNEL, "Failed to acquire JNI common class interface." );
+
+		return true;
+	}
+
+	const bool ClassRegistry::Finalize()
+	{
+		m_get_simple_name		= {};
+		m_get_name				= {};
+		m_get_canonical_name	= {};
+		m_get_super_class		= {};
+
+		m_load_class			= {};
+		m_class_loader			= {};
+
+		EXPECTS_DEBUG( m_class_storage.empty() );
+		return true;
+	}
+
+	const bool ClassRegistry::AcquireClassLoader()
+	{
+		const Black::JniClass loader_class{ Black::JniClass::FromHandleType<::Jni::ClassLoader>() };
 		CRETE( !loader_class, false, LOG_CHANNEL, "Failed to locate `java.lang.ClassLoader` class." );
 
-		m_load_class_func = { loader_class, "loadClass" };
-		CRETE( !m_load_class_func, false, LOG_CHANNEL, "Failed to locate `Class ClassLoader::findClass( String )` function." );
+		m_load_class = { loader_class, "loadClass" };
+		CRETE( !m_load_class, false, LOG_CHANNEL, "Failed to locate `Class java.lang.ClassLoader.findClass( String )` function." );
 
-		const Black::JniClass thread_class{ "java/lang/Thread" };
+		const Black::JniClass thread_class{ Black::JniClass::FromHandleType<::Jni::Thread>() };
 		CRETE( !thread_class, false, LOG_CHANNEL, "Failed to locate `java.lang.Thread` class." );
 
-		const Black::JniStaticFunction<::Jni::Thread> current_thread_func{ thread_class, "currentThread" };
-		CRETE( !current_thread_func, false, LOG_CHANNEL, "Failed to locate `Thread Thread::currentThread()` function." );
+		const Black::JniStaticFunction<::Jni::Thread ()> current_thread_func{ thread_class, "currentThread" };
+		CRETE( !current_thread_func, false, LOG_CHANNEL, "Failed to locate `Thread java.lang.Thread.currentThread()` function." );
 
 		const ::Jni::Thread current_thread{ current_thread_func.Call() };
 		CRETE( !current_thread, false, LOG_CHANNEL, "Failed to get object of current thread." );
 
-		const Black::JniMemberFunction<::Jni::ClassLoader> get_class_loader_func{ thread_class, "getContextClassLoader" };
-		CRETE( !get_class_loader_func, false, LOG_CHANNEL, "Failed to locate `ClassLoader Thread::getContextClassLoader()` function." );
+		const Black::JniMemberFunction<::Jni::ClassLoader ()> get_class_loader_func{ thread_class, "getContextClassLoader" };
+		CRETE( !get_class_loader_func, false, LOG_CHANNEL, "Failed to locate `ClassLoader java.lang.Thread.getContextClassLoader()` function." );
 
 		m_class_loader = get_class_loader_func.Call( current_thread );
 		CRETE( !m_class_loader, false, LOG_CHANNEL, "Failed to get class loader object." );
@@ -100,46 +161,53 @@ namespace
 		return true;
 	}
 
-	std::shared_ptr<_jclass> SharedClassStorage::MakeGlobalRef( Black::NotNull<jclass> local_ref, JNIEnv* local_env )
+	const bool ClassRegistry::AcquireClassInterface()
 	{
-		EXPECTS_DEBUG( local_ref != nullptr );
-		return { reinterpret_cast<jclass>( local_env->NewGlobalRef( local_ref ) ), SharedClassStorage::DeleteSharedClass };
+		const Black::JniClass class_handle{ "java/lang/Class" };
+		CRETE( !class_handle, false, LOG_CHANNEL, "Failed to locate `java.lang.Class` class." );
+
+		m_get_super_class = { class_handle, "getSuperclass" };
+		CRETE( !m_get_super_class, false, LOG_CHANNEL, "Failed to locate `Class java.lang.Class.getSuperclass()` function." );
+
+		m_get_canonical_name = { class_handle, "getCanonicalName" };
+		CRETE( !m_get_canonical_name, false, LOG_CHANNEL, "Failed to locate `String java.lang.Class.getCanonicalName()` function." );
+
+		m_get_name = { class_handle, "getName" };
+		CRETE( !m_get_name, false, LOG_CHANNEL, "Failed to locate `String java.lang.Class.getName()` function." );
+
+		m_get_simple_name = { class_handle, "getSimpleName" };
+		CRETE( !m_get_simple_name, false, LOG_CHANNEL, "Failed to locate `String java.lang.Class.getSimpleName()` function." );
+
+		return true;
 	}
 
-	std::shared_ptr<_jclass> SharedClassStorage::LoadClass( std::string_view class_name )
+	std::shared_ptr<_jclass> ClassRegistry::MakeGlobalRef( Black::NotNull<jclass> local_ref, JNIEnv* local_env )
 	{
-		JNIEnv* local_env		= Black::JniConnection::GetLocalEnvironment();
-		jclass local_class_ref	= nullptr;
+		EXPECTS_DEBUG( local_ref != nullptr );
+		return { static_cast<jclass>( local_env->NewGlobalRef( local_ref ) ), ClassRegistry::DeleteSharedClass };
+	}
+
+	std::shared_ptr<_jclass> ClassRegistry::LoadClass( std::string_view class_name )
+	{
+		JNIEnv* local_env = Black::JniConnection::GetLocalEnvironment();
+
+		jclass local_class_ref = LoadClass( class_name, local_env );
+		CRET( local_class_ref == nullptr, {} );
+
+		return MakeGlobalRef( local_class_ref, local_env );
+	}
+
+	jclass ClassRegistry::LoadClass( std::string_view class_name, JNIEnv* local_env )
+	{
+		jclass local_class_ref = nullptr;
 
 		if( Black::JniConnection::IsMainThread() )
 		{
-			char* name_buffer = static_cast<char*>( alloca( class_name.length() + 1 ) );
-			Black::CopyMemory( name_buffer, class_name.data(), class_name.length() );
-			name_buffer[ class_name.length() ] = 0;
-
-			// For the main thread we may use `FindClass` function from `JNIEnv` object.
-			local_class_ref = local_env->FindClass( name_buffer );
+			local_class_ref = LoadClassFromMainThread( class_name, local_env );
 		}
 		else
 		{
-			// Since JNI can't return class reference from native threads,
-			// according to next article the best way is using the cached `ClassLoader` instance.
-			// https://developer.android.com/training/articles/perf-jni.html#faq_FindClass
-
-			// The name of class must be converted into Java-style package name.
-			std::string jni_class_name{ class_name };
-			for( auto& item : jni_class_name )
-			{
-				if( item == '/' )
-				{
-					item = '.';
-				}
-			}
-
-			// For any other thread only captured `ClassLoader` instance may be used.
-			local_class_ref = reinterpret_cast<jclass>(
-				local_env->CallObjectMethod( *m_class_loader, *m_load_class_func, Black::ToJni( jni_class_name ) )
-			);
+			local_class_ref = LoadClassUsingClassLoader( class_name, local_env );
 		}
 
 		if( local_env->ExceptionCheck() == JNI_TRUE )
@@ -150,7 +218,37 @@ namespace
 		}
 
 		CRETW( local_class_ref == nullptr, {}, LOG_CHANNEL, "No class was found with name `{}`.", class_name );
-		return MakeGlobalRef( local_class_ref, local_env );
+		return local_class_ref;
+	}
+
+	jclass ClassRegistry::LoadClassFromMainThread( std::string_view class_name, JNIEnv* local_env )
+	{
+		EXPECTS_DEBUG( Black::JniConnection::IsMainThread() );
+
+		char* name_buffer = static_cast<char*>( alloca( class_name.length() + 1 ) );
+		Black::CopyMemory( name_buffer, class_name.data(), class_name.length() );
+		name_buffer[ class_name.length() ] = 0;
+
+		// For the main thread we may use `FindClass` function from `JNIEnv` object.
+		return local_env->FindClass( name_buffer );
+	}
+
+	jclass ClassRegistry::LoadClassUsingClassLoader( std::string_view class_name, JNIEnv* local_env )
+	{
+		// Since JNI can't return class reference from native threads,
+		// according to next article the best way is using the cached `ClassLoader` instance.
+		// https://developer.android.com/training/articles/perf-jni.html#faq_FindClass
+
+		// The name of class must be converted into Java-style package name.
+		char* name_buffer = static_cast<char*>( alloca( class_name.length() ) );
+		std::transform( class_name.begin(), class_name.end(), name_buffer, []( const char symbol ) { return ( symbol == '.' )? '/' : symbol; } );
+
+		std::string_view jni_class_name{ name_buffer, class_name.length() };
+
+		// For any other thread only captured `ClassLoader` instance may be used.
+		return static_cast<jclass>(
+			local_env->CallObjectMethod( *m_class_loader, *m_load_class, Black::ConvertToJni( jni_class_name ) )
+		);
 	}
 }
 }
