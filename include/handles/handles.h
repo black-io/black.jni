970c9b4f8fcb739ff10c69ab29c97375e2e4340c
Frank Stein <dr.frank.stain@gmail.com>
2021-02-10 21:42:09 +0300

    Main header for handles subsystem refactored.

--- include/handles/handles.h
+++ include/handles/handles.h
@@ -6,25 +6,88 @@
 #include <black/core/concurrency.h>
 
 
-// Namespace declaration.
-#include "namespace.h"
+namespace Black
+{
+inline namespace Jni
+{
+// Java object handles subsystem.
+inline namespace Handles
+{
+// Internal stuff for JNI handles.
+namespace Internal
+{
+
+}
+
+// Field handles coalition.
+inline namespace Fields
+{
+	// Alias the internal stuff.
+	namespace Internal = Black::Jni::Handles::Internal;
+}
+
+// Function handles coalition.
+inline namespace Functions
+{
+	// Alias the internal stuff.
+	namespace Internal = Black::Jni::Handles::Internal;
+}
+
+// Object handles coalition.
+inline namespace Objects
+{
+	// Alias the internal stuff.
+	namespace Internal = Black::Jni::Handles::Internal;
+}
+}
+}
+}
 
 
-// JNI handles.
-#include "JniClass.h"
-#include "marshaling/NativeTypeContext.JniClass.h"
+// Most fundamental internal stuff.
+#include "internal/forwards.h"
+#include "internal/ObjectStateBuffer.h"
+#include "internal/ObjectState.h"
 
-#include "internal/SharedStateStorage.h"
-#include "internal/SharedState.h"
-#include "JniObject.h"
-#include "marshaling/NativeTypeContext.JniObject.h"
-#include "marshaling/NativeObjectContext.h"
+// JNI objects.
+#include "objects/forwards.h"
 
-#include "JniMemberField.h"
-#include "JniStaticField.h"
+#include "objects/JniClass.h"
+#include "objects/JniObject.h"
 
-#include "MemberFunctionEntry.h"
-#include "JniMemberFunction.h"
+#include "internal/ClassHandleJniConverter.h"
+#include "internal/ObjectHandleJniConverter.h"
+#include "internal/ObjectJniConverter.h"
 
-#include "StaticFunctionEntry.h"
-#include "JniStaticFunction.h"
+// JNI field handles.
+#include "fields/JniMemberField.h"
+#include "fields/JniStaticField.h"
+
+// JNI Function handles.
+#include "internal/MemberFunctionEntry.h"
+#include "internal/StaticFunctionEntry.h"
+
+#include "functions/JniMemberFunction.h"
+#include "functions/JniStaticFunction.h"
+
+// Performance-support tools.
+#include "internal/ClassRegistry.h"
+#include "internal/ObjectStateRegistry.h"
+
+// Deferred implementations.
+#include "internal/ObjectStateRegistry.inl"
+
+#include "functions/JniStaticFunction.inl"
+#include "functions/JniMemberFunction.inl"
+
+#include "internal/StaticFunctionEntry.inl"
+#include "internal/MemberFunctionEntry.inl"
+
+#include "fields/JniStaticField.inl"
+#include "fields/JniMemberField.inl"
+
+#include "objects/JniObject.inl"
+#include "objects/JniClass.inl"
+
+#include "internal/ObjectState.inl"
+#include "internal/ObjectStateBuffer.inl"
