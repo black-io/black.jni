9bb7eca4c3daee73ebae5b176840e03ad59b83ab
Frank Stein <dr.frank.stain@gmail.com>
2021-02-10 21:43:45 +0300

    Handles subsystem published.

---
+++ include/.public/black/jni/handles.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+// The prologue of library.
+#include "../../../.prologue.h"
+
+// JNI startup subsystem.
+#include "../../../handles/handles.h"
+
+// The epilogue of library.
+#include "../../../.epilogue.h"
