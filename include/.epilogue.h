e3c9650279ac8bb5f45e0e77c1e7e17f2d88ba07
Frank Stein <dr.frank.stain@gmail.com>
2021-02-10 21:35:25 +0300

    Valid order of marshaling headers.

--- include/.epilogue.h
+++ include/.epilogue.h
@@ -1,5 +1,15 @@
 #pragma once
 
 
+// Deferred implementations.
+#include ".global/marshaling/functions.inl"
+
+#include ".global/marshaling/internal/CommonTypeJniConverter.inl"
+#include ".global/marshaling/internal/CommonTypeJniConverter.FromJni.inl"
+#include ".global/marshaling/internal/CommonTypeJniConverter.ToJni.inl"
+#include ".global/marshaling/internal/ArrayTypeConverter.inl"
+#include ".global/marshaling/internal/CommonTypeJniContext.inl"
+#include ".global/marshaling/internal/CommonTypeSignature.inl"
+
 // Restore the state of logging macros.
 #include ".bootstrap/logging.restoration.h"
--- include/.prologue.h
+++ include/.prologue.h
@@ -21,6 +21,22 @@
 #include ".global/static-strings/signatures.h"
 #include ".global/type-traits/IsBitField.h"
 
-// Some forward declarations.
-#include "virtual-machine/forwards.h"
-#include "handles/forwards.h"
+// Persistent stuff.
+#include ".global/jni-connections/internal/structures.h"
+#include ".global/jni-connections/internal/JniConnectionInitializer.h"
+#include ".global/jni-connections/JniConnection.h"
+#include ".global/jni-connections/JniEnvironment.h"
+
+// JNI Marshaling.
+#include ".global/marshaling/internal/CommonTypeSignature.h"
+#include ".global/marshaling/internal/CommonTypeJniContext.h"
+#include ".global/marshaling/internal/ArrayTypeConverter.h"
+#include ".global/marshaling/internal/CommonTypeJniConverter.h"
+
+#include ".global/marshaling/internal/EnumJniConverter.h"
+#include ".global/marshaling/internal/BitfieldJniConverter.h"
+#include ".global/marshaling/internal/EnumFlagsJniConvertter.h"
+
+#include ".global/marshaling/internal/NativeTypeConverterSelector.h"
+#include ".global/marshaling/aliases.h"
+#include ".global/marshaling/functions.h"
