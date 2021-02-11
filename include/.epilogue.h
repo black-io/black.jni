e59150294a1d2e081abbc3384f44d8b0a91ca896
Frank Stein <dr.frank.stain@gmail.com>
2021-02-11 17:33:54 +0300

    Nonexistent files removed.

--- include/.epilogue.h
+++ include/.epilogue.h
@@ -4,12 +4,9 @@
 // Deferred implementations.
 #include ".global/marshaling/functions.inl"
 
-#include ".global/marshaling/internal/CommonTypeJniConverter.inl"
 #include ".global/marshaling/internal/CommonTypeJniConverter.FromJni.inl"
 #include ".global/marshaling/internal/CommonTypeJniConverter.ToJni.inl"
 #include ".global/marshaling/internal/ArrayTypeConverter.inl"
-#include ".global/marshaling/internal/CommonTypeJniContext.inl"
-#include ".global/marshaling/internal/CommonTypeSignature.inl"
 
 // Restore the state of logging macros.
 #include ".bootstrap/logging.restoration.h"
