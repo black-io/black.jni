e7d8fa7ec4b8bd915b4fd6207e5c682bf710d0d7
Frank Stein <dr.frank.stain@gmail.com>
2021-02-11 17:31:08 +0300

    Valid type of JNI converter used.

--- include/.global/marshaling/internal/BitfieldJniConverter.h
+++ include/.global/marshaling/internal/BitfieldJniConverter.h
@@ -16,29 +16,29 @@ namespace Internal
 	struct BitfieldJniConverter : CommonTypeJniConverter<decltype( std::declval<TNativeBitfield>().bits )>
 	{
 		// Underlying type for bit-field type.
-		using UnderlyingType	= decltype( std::declval<TNativeBitfield>().bits );
+		using UnderlyingType		= decltype( std::declval<TNativeBitfield>().bits );
 
 		// JNI context for underlying type.
-		using UnderlyingContext	= CommonTypeContext<UnderlyingType>;
+		using UnderlyingConvrter	= CommonTypeJniConverter<UnderlyingType>;
 
 
 		// C++ native type.
 		using NativeType	= TNativeBitfield;
 
 		// JNI type
-		using JniType		= typename UnderlyingContext::JniType;
+		using JniType		= typename UnderlyingConvrter::JniType;
 
 
 		// Type translation from JNI space to C++ space.
 		static inline void FromJni( const JniType& source, NativeType& destination )
 		{
-			UnderlyingContext::FromJni( source, destination.bits );
+			UnderlyingConvrter::FromJni( source, destination.bits );
 		}
 
 		// Type translation from C++ space to JNI space.
 		static inline void ToJni( const NativeType& source, JniType& destination )
 		{
-			UnderlyingContext::FromJni( source.bits, destination );
+			UnderlyingConvrter::FromJni( source.bits, destination );
 		}
 	};
 }
