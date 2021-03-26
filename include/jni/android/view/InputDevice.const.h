5069cf70567a55a817340620d302d57d9fac10af
Frank Stein <dr.frank.stain@gmail.com>
2021-03-26 09:12:59 +0300

    Code style fixed.

--- include/jni/android/view/InputDevice.const.h
+++ include/jni/android/view/InputDevice.const.h
@@ -10,12 +10,12 @@ inline namespace view
 	// Translation of input device class.
 	enum class InputSourceClass : int32_t
 	{
-		None		= AINPUT_SOURCE_CLASS_NONE,				// android.view.InputDevice.SOURCE_CLASS_NONE
-		Button		= AINPUT_SOURCE_CLASS_BUTTON,			// android.view.InputDevice.SOURCE_CLASS_BUTTON
-		Pointer		= AINPUT_SOURCE_CLASS_POINTER,			// android.view.InputDevice.SOURCE_CLASS_POINTER
-		Navigation	= AINPUT_SOURCE_CLASS_NAVIGATION,		// android.view.InputDevice.SOURCE_CLASS_TRACKBALL
-		Position	= AINPUT_SOURCE_CLASS_POSITION,			// android.view.InputDevice.SOURCE_CLASS_POSITION
-		Joystick	= AINPUT_SOURCE_CLASS_JOYSTICK,			// android.view.InputDevice.SOURCE_CLASS_JOYSTICK
+		None		= AINPUT_SOURCE_CLASS_NONE,			// android.view.InputDevice.SOURCE_CLASS_NONE
+		Button		= AINPUT_SOURCE_CLASS_BUTTON,		// android.view.InputDevice.SOURCE_CLASS_BUTTON
+		Pointer		= AINPUT_SOURCE_CLASS_POINTER,		// android.view.InputDevice.SOURCE_CLASS_POINTER
+		Navigation	= AINPUT_SOURCE_CLASS_NAVIGATION,	// android.view.InputDevice.SOURCE_CLASS_TRACKBALL
+		Position	= AINPUT_SOURCE_CLASS_POSITION,		// android.view.InputDevice.SOURCE_CLASS_POSITION
+		Joystick	= AINPUT_SOURCE_CLASS_JOYSTICK,		// android.view.InputDevice.SOURCE_CLASS_JOYSTICK
 	};
 }
 }
