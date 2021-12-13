6686e3982b9b7ef8c68a3814259d4bd728f63d4c
Frank Stein <dr.frank.stain@gmail.com>
2021-12-13 22:11:16 +0300

    Accessing the services from context handle.

--- include/jni/android/content/Context.h
+++ include/jni/android/content/Context.h
@@ -31,6 +31,34 @@ inline namespace content
 		const Context& operator = ( jobject object_ref )	{ Black::JniObject::operator=( object_ref ); return *this; };
 		const Context& operator = ( const Context& other )	{ Black::JniObject::operator=( other ); return *this; };
 		const Context& operator = ( Context&& other )		{ Black::JniObject::operator=( std::move( other ) ); return *this; };
+
+	// Public interface.
+	public:
+		// Getter for `android.content.Context.DISPLAY_SERVICE` field.
+		inline std::string GetDisplayServiceName() const							{ return m_handles->display_service.GetValue( {} ); };
+
+		// Getter for `android.content.Context.WINDOW_SERVICE` field.
+		inline std::string GetWindowServiceName() const							{ return m_handles->window_service.GetValue( {} ); };
+
+
+		// Call the `android.content.Context.getSystemService` function.
+		inline Black::JniObject GetSystemService( const std::string& name ) const	{ return m_handles->get_system_service.Call( *this, name ); };
+
+	// Private inner types.
+	private:
+		struct ConfigurationState final
+		{
+			Black::JniClass	class_handle{ ClassPath::GetData() };
+
+			Black::JniStaticField<std::string>							display_service		{ class_handle, "DISPLAY_SERVICE" };
+			Black::JniStaticField<std::string>							window_service		{ class_handle, "WINDOW_SERVICE" };
+
+			Black::JniMemberFunction<Black::JniObject ( std::string )>	get_system_service	{ class_handle, "getSystemService" };
+		};
+
+	// Private state.
+	private:
+		ObjectState<ConfigurationState, true> m_handles; // Shared JNI handles.
 	};
 }
 }
