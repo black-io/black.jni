f7affa6c4106aa13004f646e053e3fd2a6bf54f1
Frank Stein <dr.frank.stain@gmail.com>
2021-12-14 10:37:03 +0300

    New public header for handles of android.view.WindowManager objects.

---
+++ include/.public/black/jni/android/view/WindowManager.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+// General dependency.
+#include <black/jni.h>
+#include <black/jni/handles.h>
+
+
+// References.
+#include <black/jni/android/view/Display.h>
+
+
+// Implementation.
+#include "../../../../../jni/android/view/WindowManager.h"
